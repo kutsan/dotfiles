1e7f1719819fe1556bac313e551e1dea283f2a7f
Kutsan Kaplan <me@kutsan.dev>
2017-11-17 18:48:52 +0300

    macos: Add initial configuration files

---
+++ .config/macos/preferences.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Enable debugging mode to see what commands executed in runtime.
+set -x
+
+# -- Global {{{1
+# --------------------------------------------------------------------------------------------------
+
+# Show all files everywhere. Even in open dialog.
+defaults write -g AppleShowAllFiles -bool true
+
+# Don't write `.DS_Store` files to portable devices.
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# -- Dock {{{1
+# --------------------------------------------------------------------------------------------------
+
+# Fully resize Dock's body.
+defaults write com.apple.dock tilesize -int 39
+
+killall Dock
+
+# }}} vim: foldmethod=marker foldlevel=0
