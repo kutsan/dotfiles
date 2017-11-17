c42c5efaec042d801bba843635df08a819e1ce70
Kutsan Kaplan <me@kutsankaplan.com>
2017-11-17 18:42:21 +0300

    browsersync: Add initial configuration file

---
+++ .config/browsersync/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	// Default port.
+	port: 3000,
+
+	// Default UI port.
+	ui: { port: 3001 },
+
+	// Watch all files from the current directory.
+	files: '*',
+
+	// Serve static files from the current directory.
+	server: true,
+
+	// Disable small pop-over notifications.
+	notify: false,
+
+	// Only serve files through localhost.
+	online: false,
+
+	// Disable clicks, scrolls and form inputs sync.
+	ghostMode: false,
+
+	// Disable defult "[Browsersync]" log prefix.
+	logPrefix: null
+}
