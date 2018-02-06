5a7f2333b110e58a038e2f412211681cebff2745
Kutsan Kaplan <me@kutsankaplan.com>
2018-02-06 05:17:37 +0300

    zsh: Move utility functions to .zsh/lib and refactor

--- .zsh/lib/console.zsh
+++ .zsh/lib/console.zsh
@@ -8,17 +8,17 @@
 # @version v0.1.0
 ##
 
-function _initialize_colors() {
+# Initialize colors.
+function () {
 	setopt LOCAL_OPTIONS UNSET
 
 	if [[ "$color" == '' ]] 2>/dev/null {
 		autoload -U colors && colors
 	}
 }
-_initialize_colors
 
 function console.error() {
-	echo -e "  ${bg[red]}${fg[white]} ERROR ${reset_color} $1" >&2
+	echo -e "  ${bg[red]}${fg[white]} error ${reset_color} $1" >&2
 
 	local i=''
 	for (( i=2; i <= ${#@}; i++ )) {
@@ -28,7 +28,7 @@ function console.error() {
 }
 
 function console.warn() {
-	echo -e "  ${bg[yellow]}${bold_color}${fg[black]} WARN ${reset_color} $1" >&2
+	echo -e "  ${bg[yellow]}${bold_color}${fg[black]} warn ${reset_color} $1" >&2
 
 	local i=''
 	for (( i=2; i <= ${#@}; i++ )) {
@@ -38,7 +38,12 @@ function console.warn() {
 }
 
 function console.info() {
-	echo -e "  ${bg[white]}${bold_color}${fg[black]} INFO ${reset_color} $1"
+	if [[ "$1" == '--with-date' ]] {
+		shift
+		echo -e "  ${bg[white]}${bold_color}${fg[black]} info ${reset_color} ${bold_color}$(date +'%Y-%m-%d %H:%M:%S')${reset_color} $1"
+	} else {
+		echo -e "  ${bg[white]}${bold_color}${fg[black]} info ${reset_color} $1"
+	}
 
 	local i=''
 	for (( i=2; i <= ${#@}; i++ )) {
