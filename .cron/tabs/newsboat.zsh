f28a86156e81ba4497820014a7bec1ff34acebe0
Kutsan Kaplan <me@kutsan.dev>
2018-02-06 05:21:16 +0300

    zsh: Add logging functionality to newsboat.zsh task

--- .cron/tabs/newsboat.zsh
+++ .cron/tabs/newsboat.zsh
@@ -3,5 +3,14 @@
 # Load environment variables.
 source ~/.zsh/config/exports.zsh
 
+# Load utility functions for logging.
+source ~/.zsh/lib/console.zsh
+
+# Redirect all output to log file.
+exec &>> $CRONTAB_HOME/logs/$(basename $0).log
+
+# Log with date to indicate it's running in success.
+console.info --with-date "Running."
+
 # Reload feeds and that will launch notify-program if there are new feeds.
 newsboat -x reload
