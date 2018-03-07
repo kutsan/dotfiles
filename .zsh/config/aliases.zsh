d3dcf00d607f3efac772fd7fa7838f85790db217
Kutsan Kaplan <me@kutsan.dev>
2018-03-07 05:48:59 +0300

    zsh: Update config aliases

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -28,7 +28,7 @@ alias tmux='tmux -f "$HOME/.tmux/tmux.conf"'
 alias dfc='dfc -dmT' # -d: Show used size. -m: Base 10 output. -T: Show file system types.
 alias cdu='cdu --intelligent --log --reverse --sort --du h'
 alias syncthing='syncthing -home=$HOME/.config/syncthing'
-alias exa='exa --color=always --all --classify --group-directories-first --long --git --group'
+alias la='exa --color=always --all --classify --group-directories-first --long --git --group'
 alias serve='browser-sync start --config ~/.config/browsersync/config.js'
 alias jq='jq --tab --indent 4'
 alias news='newsboat --quiet'
@@ -69,36 +69,36 @@ alias -g R='| rg'
 alias -g F='| fzf'
 
 # Configs
-alias c-alacritty="$EDITOR ~/.config/alacritty/alacritty.yml"
-alias c-browsersync="$EDITOR ~/.config/browsersync/config.js"
-alias c-crontab="$EDITOR ~/.cron/crontab"
+alias c-alacritty="$EDITOR +'cd ~/.config/alacritty/' ~/.config/alacritty/alacritty.yml"
+alias c-browsersync="$EDITOR +'cd ~/.config/browsersync/' ~/.config/browsersync/config.js"
+alias c-crontab="$EDITOR +'cd ~/.cron/' ~/.cron/crontab"
 alias c-editorconfig="$EDITOR ~/.editorconfig"
-alias c-emacs="$EDITOR ~/.emacs.d/init.el"
-alias c-git="$EDITOR ~/.config/git/"
-alias c-gpg="$EDITOR ~/.gnupg/gpg.conf"
-alias c-htop="$EDITOR ~/.config/htop/htoprc"
-alias c-karabiner="$EDITOR ~/.config/karabiner/karabiner.json"
-alias c-less="$EDITOR $HOME/.config/less/"
-alias c-mac="$EDITOR ~/.config/macos/"
-alias c-mailcap="$EDITOR ~/.mutt/mailcap"
-alias c-mpv="$EDITOR ~/.config/mpv/"
-alias c-msmtp="$EDITOR ~/.mutt/msmtprc"
-alias c-mutt="$EDITOR ~/.mutt/muttrc"
-alias c-newsboat="$EDITOR ~/.newsboat/"
-alias c-ranger="$EDITOR ~/.config/ranger/"
+alias c-emacs="$EDITOR +'cd ~/.emacs.d/' ~/.emacs.d/init.el"
+alias c-git="$EDITOR +'cd ~/.config/git/' ~/.config/git/"
+alias c-gpg="$EDITOR +'cd ~/.gnupg/' ~/.gnupg/gpg.conf"
+alias c-htop="$EDITOR +'cd ~/.config/htop/' ~/.config/htop/htoprc"
+alias c-karabiner="$EDITOR +'cd ~/.config/karabiner/' ~/.config/karabiner/karabiner.json"
+alias c-less="$EDITOR +'cd ~/.config/less/' $HOME/.config/less/"
+alias c-mac="$EDITOR +'cd ~/.config/macos/' ~/.config/macos/"
+alias c-mailcap="$EDITOR +'cd ~/.mutt/' ~/.mutt/mailcap"
+alias c-mpv="$EDITOR +'cd ~/.config/mpv/' ~/.config/mpv/"
+alias c-msmtp="$EDITOR +'cd ~/.mutt/' ~/.mutt/msmtprc"
+alias c-mutt="$EDITOR +'cd ~/.mutt/' ~/.mutt/muttrc"
+alias c-newsboat="$EDITOR +'cd ~/.newsboat/' ~/.newsboat/"
+alias c-ranger="$EDITOR +'cd ~/.config/ranger/' ~/.config/ranger/"
 alias c-readme="$EDITOR ~/README.md"
-alias c-ssh="$EDITOR ~/.ssh/config"
-alias c-stig="$EDITOR ~/.config/stig/rc"
+alias c-ssh="$EDITOR +'cd ~/.ssh/' ~/.ssh/config"
+alias c-stig="$EDITOR +'cd ~/.config/stig/' ~/.config/stig/rc"
 alias c-task="$EDITOR ~/.taskrc"
-alias c-termux="$EDITOR ~/.termux/"
-alias c-tmux="$EDITOR ~/.tmux/"
-alias c-transmission="$EDITOR ~/.config/transmission/"
-alias c-vim="$EDITOR ~/.vim/init.vim"
-alias c-wget="$EDITOR ~/.config/wget/wgetrc"
-alias c-youtube-dl="$EDITOR ~/.config/youtube-dl/config"
-alias c-zsh="$EDITOR $ZDOTDIR/.zshrc" r-zsh="source $ZDOTDIR/.zshrc"
+alias c-termux="$EDITOR +'cd ~/.termux/' ~/.termux/"
+alias c-tmux="$EDITOR +'cd ~/.tmux/' ~/.tmux/"
+alias c-transmission="$EDITOR +'cd ~/.config/transmission/' ~/.config/transmission/"
+alias c-vim="$EDITOR +'cd ~/.vim/' ~/.vim/init.vim"
+alias c-wget="$EDITOR +'cd ~/.config/wget/' ~/.config/wget/wgetrc"
+alias c-youtube-dl="$EDITOR +'cd ~/.config/youtube-dl/' ~/.config/youtube-dl/config"
+alias c-zsh="$EDITOR +'cd $ZDOTDIR' $ZDOTDIR/.zshrc" r-zsh="source $ZDOTDIR/.zshrc"
 alias c-zshenv="$EDITOR ~/.zshenv"
-alias c-zshhistory="$EDITOR $HISTFILE"
+alias c-zshhistory="$EDITOR +'cd $ZDOTDIR' $HISTFILE"
 
 # Git
 alias g='git'
