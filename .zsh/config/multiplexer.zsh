d818d1610ff42ce875254e870101c686fc31c49e
Kutsan Kaplan <me@kutsankaplan.com>
2018-03-07 21:11:05 +0300

    zsh: Launch multiplexer under neovim without status bar

--- .zsh/config/multiplexer.zsh
+++ .zsh/config/multiplexer.zsh
@@ -1,26 +1,42 @@
-# Auto-attach tmux or start at launch with different sessions based on platforms.
-if (
-	hash tmux 2>/dev/null \
-	&& [[ $TMUX == '' ]] \
-	&& [[ $SUDO_USER == '' ]] \
-	&& [[ $EMACS == '' ]] \
-	&& [[ $VIM == '' ]] \
-	&& [[ $SSH_CONNECTION == '' ]] \
-	&& [[ $(uname -o) != 'Android' ]] 2>/dev/null
-) {
-	typeset -g session=''
+function should_launch_multiplexer() {
+	if (! hash tmux 2>/dev/null) {
+		return 1
+	}
 
-	# VSCode integrated terminal
+	if [[ $NVIM_LISTEN_ADDRESS != '' ]] {
+		return 0
+	} elif (
+		[[ $TMUX == '' ]] \
+		&& [[ $SUDO_USER == '' ]] \
+		&& [[ $EMACS == '' ]] \
+		&& [[ $SSH_CONNECTION == '' ]] \
+		&& [[ $(uname -o) != 'Android' ]] 2>/dev/null
+	) {
+		return 0
+	}
+
+	return 1
+}
+
+# Auto-attach tmux or start at launch with different sessions based on platforms.
+if (should_launch_multiplexer) {
+	typeset -g session='main'
+	typeset -g launch_options=''
+
 	if [[ $VSCODE_PID != '' ]] {
 		session='vscode'
 
-	# Terminal emulator
-	} else {
-		session='main'
+	} elif [[ $NVIM_LISTEN_ADDRESS != '' ]] {
+		session='nvim'
+		launch_options='\; set-option -w status off'
 	}
 
-	tmux attach-session -t "$session" \
-		|| tmux -f "$HOME/.tmux/tmux.conf" new -s "$session" && exit 0
+	tmux attach-session -t "$session" 2>/dev/null \
+		|| eval tmux -f "$HOME/.tmux/tmux.conf" new-session -s "$session" $launch_options \
+		&& exit 0
 
 	unset session
+	unset launch_options
 }
+
+unset -f should_launch_multiplexer
