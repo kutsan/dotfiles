a829be02bf188c277cbc9570803931378df42b97
Kutsan Kaplan <me@kutsankaplan.com>
2018-04-07 14:46:45 +0300

    zsh: Fix typing style for prompt.zsh

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -1,20 +1,20 @@
 # Initialize prompt system.
 promptinit
 
-# Prompt theme
+# Prompt theme.
 prompt pure
 
-# Prompt options
+# Prompt options.
 typeset -g PROMPT_CURRENT_SYMBOL="$PROMPT_INSERT_MODE_SYMBOL"
 typeset -g PROMPT_INSERT_MODE_SYMBOL=''
 typeset -g PROMPT_NORMAL_MODE_SYMBOL='▢'
 typeset -g PROMPT_NORMAL_STATUS_COLOR='white'
 typeset -g PROMPT_ERROR_STATUS_COLOR='red'
 
-# Primary Prompt
+# Primary prompt.
 PROMPT='%(?.%F{$PROMPT_NORMAL_STATUS_COLOR}.%F{$PROMPT_ERROR_STATUS_COLOR})${PROMPT_CURRENT_SYMBOL}%f '
 
-# Spelling Correction Prompt
+# Spelling correction prompt.
 SPROMPT="zsh: correct %F{red}'%R'%f to %F{red}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
 
 # ZLE hooks for prompt's Vim mode status
