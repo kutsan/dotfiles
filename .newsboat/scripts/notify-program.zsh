991f4f0e36fee9b2a681f7456db50aed2d32b812
Kutsan Kaplan <me@kutsan.dev>
2018-05-19 23:53:57 +0300

    newsboat: Drop console.zsh dependency

--- .newsboat/scripts/notify-program.zsh
+++ .newsboat/scripts/notify-program.zsh
@@ -5,11 +5,8 @@ exec &>> ~/.newsboat/logs/$(basename $0).log
 
 setopt ERR_EXIT PIPE_FAIL WARN_CREATE_GLOBAL WARN_NESTED_VAR
 
-# Import utility functions for logging.
-source ~/.zsh/lib/console.zsh
-
 if (! hash notify &>/dev/null) {
-	console.error --with-date "${bold_color}notify${reset_color} is not installed yet."
+	echo "[$(date +'%Y-%m-%d %H:%M:%S')] notify is not installed yet."
 	exit 1
 }
 
