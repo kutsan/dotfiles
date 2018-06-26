4b02fbc31d2830ec19f7c7300cf35cfdc87c58d1
Kutsan Kaplan <me@kutsan.dev>
2018-06-26 22:31:58 +0300

    zsh: Use registerable hook functions

--- .zsh/config/hooks.zsh
+++ .zsh/config/hooks.zsh
@@ -1,3 +1,26 @@
+# Register hook functions.
+foreach hook (
+	'preexec _preexec_ssh'
+	'preexec _preexec_fasd'
+) {
+	eval add-zsh-hook $hook
+}
+unset hook
+
+# Transform the cursor to box form on ssh command.
+function _preexec_ssh() {
+	if [[ "$2" =~ '^ssh[[:space:]]' ]] {
+		printf '\e[0 q'
+	}
+}
+
+# Register fasd to track most used files and directories.
+function _preexec_fasd() {
+	if (hash fasd &>/dev/null) {
+		fasd --proc $(fasd --sanitize "$1") &>/dev/null
+	}
+}
+
 # Compile lesskey file if compile is needed.
 if ([[ ! -f $LESSKEY ]] || [[ $LESSKEYRC -nt $LESSKEY ]]) {
 	lesskey -o $LESSKEY $LESSKEYRC
@@ -23,19 +46,3 @@ if ! [[ -L $ZDOTDIR/autoload/async ]] {
 zrecompile -p \
 	-M $ZDOTDIR/autoload/async -- \
 	-M $ZDOTDIR/autoload/prompt_pure_setup
-
-# Transform the cursor to box form on ssh command.
-function _ssh_preexec() {
-	if [[ "$2" =~ '^ssh[[:space:]]' ]] {
-		printf '\e[0 q'
-	}
-}
-add-zsh-hook preexec _ssh_preexec
-
-# Register fasd to track most used files and directories.
-function _fasd_preexec() {
-	if (hash fasd &>/dev/null) {
-		fasd --proc $(fasd --sanitize "$1") &>/dev/null
-	}
-}
-add-zsh-hook preexec _fasd_preexec
