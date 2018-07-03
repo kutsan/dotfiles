5a19fdaa44a6fe53dcc1eedd21278569c3a8b481
Kutsan Kaplan <me@kutsankaplan.com>
2018-07-03 21:52:06 +0300

    prettier: Rename .prettierrc.json to .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'always',
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: 'preserve'
+}
