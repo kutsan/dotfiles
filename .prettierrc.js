a994cbef97438e8ba8900904037d8559ff681c5f
Kutsan Kaplan <me@kutsankaplan.com>
2018-07-20 03:48:26 +0300

    prettier: Use configs from node_modules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,1 @@
-module.exports = {
-	printWidth: 100,
-	tabWidth: 4,
-	useTabs: true,
-	semi: false,
-	singleQuote: true,
-	trailingComma: 'none',
-	bracketSpacing: true,
-	jsxBracketSameLine: false,
-	arrowParens: 'always',
-	requirePragma: false,
-	insertPragma: false,
-	proseWrap: 'preserve'
-}
+module.exports = require('@kutsan/config/prettier')
