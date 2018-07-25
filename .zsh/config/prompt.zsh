e8b37f97c650caefaca98a1b753b299d92406100
Kutsan Kaplan <me@kutsan.dev>
2018-07-25 05:25:25 +0300

    zsh: Use static prompt symbol

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -4,17 +4,10 @@ promptinit
 # Prompt theme.
 prompt pure
 
-# Prompt options.
-typeset -g PROMPT_CURRENT_SYMBOL="$PROMPT_INSERT_MODE_SYMBOL"
-typeset -g PROMPT_INSERT_MODE_SYMBOL='❯'
-typeset -g PROMPT_NORMAL_MODE_SYMBOL='▢'
-typeset -g PROMPT_NORMAL_STATUS_COLOR='white'
-typeset -g PROMPT_ERROR_STATUS_COLOR='red'
+# Primary prompt with additional tweaks.
+typeset -g PROMPT='%(?.%F{white}.%F{red})❯%f '
 
-# Primary prompt.
-PROMPT='%(?.%F{$PROMPT_NORMAL_STATUS_COLOR}.%F{$PROMPT_ERROR_STATUS_COLOR})${PROMPT_CURRENT_SYMBOL}%f '
-
-# ZLE hooks for prompt's Vim mode status
+# ZLE hooks for prompt's vi mode status
 function zle-line-init zle-keymap-select {
 	# Change the cursor style depending on keymap mode.
 	if [[ "$SSH_CONNECTION" == '' ]] {
@@ -28,13 +21,6 @@ function zle-line-init zle-keymap-select {
 				;;
 		}
 	}
-
-	# Change the prompt symbol depending on keymap mode.
-	PROMPT_CURRENT_SYMBOL=${${KEYMAP/vicmd/${PROMPT_NORMAL_MODE_SYMBOL}}/(main|viins)/${PROMPT_INSERT_MODE_SYMBOL}}
-
-	# Redraw if necessary.
-	zle reset-prompt
-	zle -R
 }
 zle -N zle-line-init
 zle -N zle-keymap-select
