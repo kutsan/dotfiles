891cedab19dfd9781df23b215af5796e5e60a5af
Kutsan Kaplan <me@kutsan.dev>
2019-03-09 13:31:08 +0300

    ranger: Replace compress command with simple mappings

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -122,39 +122,6 @@ class jump(Command):
             else:
                 self.fm.select_file(fzf_file)
 
-class compress(Command):
-    """
-    :compress
-
-    Compress marked files to current directory with `atool`.
-    """
-
-    def execute(self):
-        cwd = self.fm.thisdir
-        marked_files = cwd.get_selection()
-
-        if not marked_files:
-            return
-
-        def refresh(_):
-            cwd = self.fm.get_directory(original_path)
-            cwd.load_content()
-
-            original_path = cwd.path
-            parts = self.line.split()
-            au_flags = parts[1:]
-
-            descr = "Compressing files in: " + os.path.basename(parts[1])
-            obj = CommandLoader(args=['apack'] + au_flags + \
-                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
-
-            obj.signal_bind('after', refresh)
-            self.fm.loader.add(obj)
-
-        def tab(self):
-            extension = ['.zip', '.tar.gz', '.rar', '.7z']
-            return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
-
 class toggle_alternate_view(Command):
     """
     :toggle_alternate_view
