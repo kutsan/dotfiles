81416124b45fa4c0fa62f670c1d123746abe4cc8
Kutsan Kaplan <me@kutsan.dev>
2020-05-14 13:25:56 +0300

    git: Add git reset alias to gr

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -30,7 +30,7 @@ alias -g L='| less'
 alias -g G='| grep'
 
 # git
-foreach aliaskey (a b bl c co d ds dt dts f l s sm w) {
+foreach aliaskey (a b bl c co d ds dt dts f l r s sm w) {
 	alias g$aliaskey="git $aliaskey"
 }
 unset aliaskey
