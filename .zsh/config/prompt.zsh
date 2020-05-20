27ed7878e6de577a052642ba64b74a329aefe962
Kutsan Kaplan <me@kutsan.dev>
2020-05-21 00:41:04 +0300

    zsh: Remove vertical and box bar modes for line editor

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -14,19 +14,3 @@ prompt pure
 # Prompt styles.
 zstyle :prompt:pure:prompt:error color 1
 zstyle :prompt:pure:prompt:success color 15
-
-# ZLE hooks for prompt's vi mode status
-function zle-line-init zle-keymap-select {
-	# Change the cursor style depending on keymap mode.
-	case $KEYMAP {
-		vicmd)
-			printf '\e[0 q' # Box.
-			;;
-
-		viins|main)
-			printf '\e[6 q' # Vertical bar.
-			;;
-	}
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
