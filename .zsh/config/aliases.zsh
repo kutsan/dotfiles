2678ca56527ace25808c91efaa57a08aa54d485f
Kutsan Kaplan <me@kutsan.dev>
2020-12-09 09:25:08 +0300

    zsh: Add alias for sync repository

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -19,8 +19,9 @@ alias bwunlock='export BW_SESSION="$(bw unlock --raw $(gpg --no-tty --for-your-e
 alias pacman='sudo pacman'
 alias rsync='rsync --protect-args --compress --verbose --progress --human-readable'
 alias l="ledger balance '^assets' '^liabilities' --empty --flat"
-alias le='$EDITOR ~/Sync/finance/journal-$(date +%Y).ledger'
+alias le='$EDITOR ~/Projects/sync/finance/journal-$(date +%Y).ledger'
 alias lnet="ledger register '^expenses' '^income' --period 'this month' --subtotal --invert"
+alias s='$EDITOR ~/Projects/sync/notes/tasks.yml'
 alias gen-npm='cp "$HOME/.config/npm/package.json" "package.json"'
 alias gen-editorconfig='cp "$HOME/.editorconfig" ".editorconfig"'
 
