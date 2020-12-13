d569df3eb6254a148a77032fd1ac3e0f36f3f53f
Kutsan Kaplan <me@kutsan.dev>
2020-12-13 05:46:24 +0300

    vim: heavily refactor toggle terminal code

--- .vim/lua/kutsan/mappings/normal/terminal.lua
+++ .vim/lua/kutsan/mappings/normal/terminal.lua
@@ -1,200 +1,83 @@
 -- Toggle floating terminal or create new one if there is none.
--- Modified version of @numToStr's FTerm.nvim plugin.
 --
 -- tnoremap <silent> <C-z> <C-\><C-n>:lua require('kutsan.mappings.normal.terminal').toggle()<CR>
 -- nnoremap <silent> <C-z> :lua require('kutsan.mappings.normal.terminal').toggle()<CR>
 
-
 local api = vim.api
 local fn = vim.fn
-local cmd = vim.cmd
 
-Terminal = {}
+local terminal = {
+	buf = nil,
+	win = nil,
+	pid = nil
+}
 
--- Init
-function Terminal:new()
-	x = {
-		wins = {},
-		bufs = {},
-	}
+terminal.open = function()
+	-- Create buffer.
+	local buf = nil
 
-	self.__index = self
-	return setmetatable(x, self)
-end
-
--- Terminal:store adds the given floating windows and buffer to the list
-function Terminal:store(name, win, buf)
-	self.wins[name] = win
-	self.bufs[name] = buf
-end
-
-
--- Terminal:create_buf creates a scratch buffer for floating window to consume
-function Terminal:create_buf(name, do_border, height, width)
-	-- If previous buffer exists then return it
-	local prev = self.bufs[name]
-	if prev and api.nvim_buf_is_loaded(prev) then
-		return prev
+	if terminal.buf and api.nvim_buf_is_loaded(terminal.buf) then
+		buf = terminal.buf
+	else
+		buf = api.nvim_create_buf(false, true)
 	end
 
-	local buf = api.nvim_create_buf(false, true)
+	-- Create window.
+	local width = math.ceil(vim.o.columns * 0.8)
+	local height = math.ceil(vim.o.lines * 0.9)
 
-	if do_border then
-		-- ## Border start ##
-		local border_lines = { '╭' .. string.rep('─', width) .. '╮' }
-		for i=1, height do
-			table.insert(border_lines, '│' .. string.rep(' ', width) .. '│')
-		end
-		table.insert(border_lines, '╰' .. string.rep('─', width) .. '╯')
-		-- ## Border end ##
-
-		api.nvim_buf_set_lines(buf, 0, -1, false, border_lines)
-	end
-
-	return buf
-end
-
--- Terminal:win_dim return window dimensions
-function Terminal:win_dim()
-	-- get dimensions
-	local cl = vim.o.columns
-	local ln = vim.o.lines
-
-	-- calculate our floating window size
-	local width = math.ceil(cl * 0.8)
-	local height = math.ceil(ln * 0.9)
-
-	-- and its starting position
-	local col = math.ceil((cl - width) / 2)
-	local row = math.ceil((ln - height) / 2 - 1)
-
-	return {
-		width = width,
-		height = height,
-		col = col,
-		row = row,
-	}
-end
-
-function Terminal:win_opts(dim)
-	return {
+	local win = api.nvim_open_win(buf, true, {
 		relative = 'editor',
 		style = 'minimal',
-		width = dim.width + 2,
-		height = dim.height + 2,
-		col = dim.col - 1,
-		row = dim.row - 1,
-	}
+		width = width,
+		height = height,
+		col = math.ceil((vim.o.columns - width) / 2),
+		row = math.ceil((vim.o.lines - height) / 2 - 1),
+	})
+	api.nvim_win_set_option(win, 'winhighlight', 'Normal:CursorLine')
+
+	-- Launch terminal.
+	if not terminal.buf then
+		terminal.pid = fn.termopen(string.format('%s --login', os.getenv('SHELL')))
+	end
+
+	vim.cmd('startinsert')
+	vim.cmd("autocmd! TermClose <buffer> lua require('kutsan.mappings.normal.terminal').close(true)")
+
+	-- Save current handles.
+	terminal.win = win
+	terminal.buf = buf
 end
 
--- Terminal:create_win creates a new window with a given buffer
-function Terminal:create_win(name, buf, opts, do_hl)
-	local win_handle = api.nvim_open_win(buf, true, opts)
-
-	if do_hl then
-		fn.nvim_win_set_option(win_handle, 'winhl', 'Normal:Normal')
+terminal.close = function(force)
+	if not terminal.win then
+		return
 	end
 
-	return win_handle
-end
-
--- Terminal:term opens a terminal inside a buffer
-function Terminal:term(t)
-	if vim.tbl_isempty(self.bufs) then
-		-- This function fails if the current buffer is modified (all buffer contents are destroyed).
-		local pid = fn.termopen(os.getenv('SHELL') .. ' --login')
-
-		-- IDK what to do with this now, maybe later we can use it
-		self.terminal = pid
+	if api.nvim_win_is_valid(terminal.win) then
+		api.nvim_win_close(terminal.win, false)
+		terminal.win = nil
 	end
 
-	cmd("startinsert")
-
-	function on_close()
-		self:close(true)
-	end
-
-	-- This fires when someone executes `exit` inside term
-	-- So, in this case the buffer should also be removed instead of reusing
-	cmd("autocmd! TermClose <buffer> lua on_close()")
-end
-
-function Terminal:open()
-	-- remember_cursor()
-	self.last_win = fn.winnr()
-	self.last_pos = fn.getpos('.')
-
-	local dim = self:win_dim()
-	local opts = self:win_opts(dim)
-
-	local bg_buf = self:create_buf('bg', true, dim.height, dim.width)
-	local bg_win = self:create_win('bg', bg_buf, opts, true)
-
-	opts.width = dim.width
-	opts.height = dim.height
-	opts.col = dim.col
-	opts.row = dim.row
-
-	local buf = self:create_buf('fg')
-	local win = self:create_win('fg', buf, opts)
-
-	self:term()
-
-	-- Need to store the handles after opening the terminal
-	self:store('bg', bg_win, bg_buf)
-	self:store('fg', win, buf)
-end
-
-function Terminal:close(force)
-	if not next(self.wins) then
-		do return end
-	end
-
-	for _, win in pairs(self.wins) do
-		if api.nvim_win_is_valid(win) then
-			api.nvim_win_close(win, {})
-		end
-	end
-
-	self.wins = {}
-
+	-- Force close upon terminal exit.
 	if force then
-		for _, buf in pairs(self.bufs) do
-			if api.nvim_buf_is_loaded(buf) then
-				-- api.nvim_buf_delete(buf, {})
-				cmd(buf..'bd!')
-			end
+		if api.nvim_buf_is_loaded(terminal.buf) then
+			api.nvim_buf_delete(terminal.buf, { force = true })
 		end
 
-		fn.jobstop(self.terminal)
+		fn.jobstop(terminal.pid)
 
-		self.bufs = {}
-		self.terminal = nil
-	end
-
-	-- restore-cursor
-	if self.last_win and next(self.last_pos) then
-		cmd(self.last_win.." wincmd w")
-		fn.setpos('.', self.last_pos)
-
-		self.last_win = nil
-		self.last_pos = nil
+		terminal.buf = nil
+		terminal.pid = nil
 	end
 end
 
-function Terminal:toggle()
-	if vim.tbl_isempty(self.wins) then
-		self:open()
+terminal.toggle = function()
+	if not terminal.win then
+		terminal.open()
 	else
-		self:close()
+		terminal.close()
 	end
 end
 
-local instance = Terminal:new()
-local M = {}
-
-function M.toggle()
-	instance:toggle()
-end
-
-return M
+return terminal
