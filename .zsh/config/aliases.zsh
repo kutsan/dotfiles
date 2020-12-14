60e191b93b4bf7c92fc80a340cd41c9dea967718
Kutsan Kaplan <me@kutsan.dev>
2020-12-14 23:03:00 +0300

    zsh: don't use --flat for l ledger alias

--- .zsh/config/aliases.zsh
+++ .zsh/config/aliases.zsh
@@ -18,7 +18,7 @@ alias bwlogin='bw login --code "$(auth bitwarden)" "$(< ~/.mutt/accounts/com.gma
 alias bwunlock='export BW_SESSION="$(bw unlock --raw $(gpg --no-tty --for-your-eyes-only --quiet --decrypt ~/.bitwarden/secret.gpg))"'
 alias pacman='sudo pacman'
 alias rsync='rsync --protect-args --compress --verbose --progress --human-readable'
-alias l="ledger balance '^assets' '^liabilities' --empty --flat"
+alias l="ledger balance '^assets' '^liabilities' --empty"
 alias le='$EDITOR ~/Projects/sync/finance/journal-$(date +%Y).ledger'
 alias lnet="ledger register '^expenses' '^income' --period 'this month' --subtotal --invert"
 alias s='$EDITOR ~/Projects/sync/notes/tasks.yml'
