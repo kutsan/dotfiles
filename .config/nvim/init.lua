155387ad16ff2d0c7907a9347f19e7147d07f651
Kutsan Kaplan <me@kutsan.dev>
2020-12-18 04:52:25 +0300

    nvim: let neovim handle g:clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 local g = vim.g
 local cmd = vim.cmd
-local fn = vim.fn
 
 -- Enable loading {ftdetect,ftplugin,indent}/*.vim files.
 cmd('filetype plugin indent on')
@@ -26,32 +25,3 @@ g.loaded_getscriptPlugin = true
 g.loaded_logipat = true
 g.loaded_tutor_mode_plugin = true
 g.loaded_matchit = true
-
--- Set internal g:clipboard to save some startup time.
-if fn.has('mac') then
-	g.clipboard = {
-		name = 'pbcopy',
-		cache_enabled = false,
-		copy = {
-		  ['+'] = 'pbcopy',
-		  ['*'] = 'pbcopy'
-		},
-		paste = {
-		  ['+'] = 'pbpaste',
-		  ['*'] = 'pbpaste'
-		}
-	}
-elseif not fn.empty('$DISPLAY') and fn.executable('xclip') then
-	g.clipboard = {
-		name = 'xclip',
-		cache_enabled = false,
-		copy = {
-		  ['+'] = 'xclip -quiet -i -selection clipboard',
-		  ['*'] = 'xclip -quiet -i -selection primary'
-		},
-		paste = {
-		  ['+'] = 'xclip -o -selection clipboard',
-		  ['*'] = 'xclip -o -selection primary'
-		}
-	}
-end
