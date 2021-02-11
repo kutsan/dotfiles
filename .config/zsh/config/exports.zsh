e422736deaa9f66663a5cbed594d0a3f4217e134
Kutsan Kaplan <me@kutsan.dev>
2021-02-12 01:01:58 +0300

    zsh: set FZF_DEFAULT_COMMAND to search files with `find`

--- .config/zsh/config/exports.zsh
+++ .config/zsh/config/exports.zsh
@@ -78,6 +78,10 @@ export GPG_TTY=$(tty)
 export GNUPGHOME="$HOME/.config/gnupg"
 
 # fzf
+export FZF_DEFAULT_COMMAND="\
+  find . \
+  \( -type d -name 'node_modules' -o -type d -name '.git' \) -prune \
+  -o -type f -print"
 export FZF_DEFAULT_COLORS='--color=dark,fg:-1,bg:-1,hl:4,fg+:7,bg+:8,gutter:-1,hl+:4,info:8,border:8,prompt:4,pointer:3,marker:3,spinner:8,header:6'
 export FZF_DEFAULT_OPTS="\
   $FZF_DEFAULT_COLORS \
