d0857b48ceba55fdda438ab34f9b835a93a62a04
Kutsan Kaplan <me@kutsankaplan.com>
2021-09-04 17:44:02 +0300

    feat(nvim): set hlsearch to default and use nohlsearch

--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -90,7 +90,6 @@ opt.grepprg = 'grep ' -- Program to use for the :grep command.
 opt.grepformat = '%f:%l:%c:%m,%f:%l:%m' -- Format to recognize for the :grep command output.
 opt.ignorecase = true -- Ignore case in search patterns.
 opt.smartcase = true -- Set 'noignorecase' if search pattern contains an uppercase letter.
-opt.hlsearch = false -- Disable highlight search matches.
 opt.undolevels = 100000 -- Maximum number of changes that can be undone.
 opt.hidden = true -- Buffers becomes hidden when they are abandoned.
 opt.splitbelow = true -- Splitting a window will put the new window below of the current one.
