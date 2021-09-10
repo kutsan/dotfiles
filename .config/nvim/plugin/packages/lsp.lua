d718fd877c173ff2a16e0e479b44930d38962e16
Kutsan Kaplan <me@kutsan.dev>
2021-09-10 09:10:05 +0300

    feat(nvim): add capabilities to cssls config

--- .config/nvim/plugin/packages/lsp.lua
+++ .config/nvim/plugin/packages/lsp.lua
@@ -56,8 +56,18 @@ local function on_attach()
   buf_map('i', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 end
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    'documentation',
+    'detail',
+    'additionalTextEdits',
+  },
+}
+
 lspconfig.tsserver.setup({ on_attach = on_attach })
-lspconfig.cssls.setup({ on_attach = on_attach })
+lspconfig.cssls.setup({ on_attach = on_attach, capabilities = capabilities })
 lspconfig.html.setup({ on_attach = on_attach })
 lspconfig.jsonls.setup({})
 
