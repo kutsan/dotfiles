93a5ee65f8f4e9c19d39f92c26d013e5257681aa
Kutsan Kaplan <me@kutsan.dev>
2021-10-27 15:25:13 +0300

    feat(kitty): use stack layout by default

--- .config/kitty/new_window.py
+++ .config/kitty/new_window.py
@@ -7,8 +7,7 @@ from kittens.tui.handler import result_handler
 def handle_result(args, answer, target_window_id, boss):
     tab = boss.active_tab
 
-    if tab is not None:
-        if tab.current_layout.name == 'stack':
-            tab.last_used_layout()
-        else:
-            tab.goto_layout('stack')
+    if len(tab) == 1:
+        tab.new_window(cwd_from=tab.active_window_for_cwd.child.pid_for_cwd)
+    else:
+        tab.next_window()
