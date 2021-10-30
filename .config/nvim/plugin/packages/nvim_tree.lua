fa58113c4417d13e8bfae777c59917995e94aa24
Kutsan Kaplan <me@kutsan.dev>
2021-10-30 21:52:52 +0300

    feat(nvim): migrate to new setup function config for nvim-tree

--- .config/nvim/plugin/packages/nvim_tree.lua
+++ .config/nvim/plugin/packages/nvim_tree.lua
@@ -1,4 +1,4 @@
-local tree_cmd = require('nvim-tree/config').nvim_tree_callback
+local tree_cb = require('nvim-tree/config').nvim_tree_callback
 local map = require('kutsan/utils').map
 local api = vim.api
 local g = vim.g
@@ -42,26 +42,41 @@ g.nvim_tree_indent_markers = 1
 -- Disable special files.
 g.nvim_tree_special_files = {}
 
--- Set key bindings.
-g.nvim_tree_bindings = {
-  { key = 'l', cb = tree_cmd('edit') },
-  { key = 'h', cb = tree_cmd('close_node') },
-  { key = 'H', cb = tree_cmd('dir_up') },
-  { key = 'L', cb = tree_cmd('cd') },
-  { key = '<C-s>', cb = tree_cmd('split') },
-  { key = '<C-v>', cb = tree_cmd('vsplit') },
-  { key = '<C-t>', cb = tree_cmd('tabnew') },
-  { key = '<C-r>', cb = tree_cmd('refresh') },
-  { key = 'i', cb = tree_cmd('preview') },
-  { key = 'c', cb = tree_cmd('create') },
-  { key = 'D', cb = tree_cmd('remove') },
-  { key = 'r', cb = tree_cmd('rename') },
-  { key = 'd', cb = tree_cmd('cut') },
-  { key = 'y', cb = tree_cmd('copy') },
-  { key = 'p', cb = tree_cmd('paste') },
-  { key = '[c', cb = tree_cmd('prev_git_item') },
-  { key = ']c', cb = tree_cmd('next_git_item') },
-}
+local nvim_tree = require('nvim-tree')
+nvim_tree.setup({
+  view = {
+    hide_root_folder = false,
+    auto_resize = true,
+    mappings = {
+      custom_only = true,
+      list = {
+        { key = { 'l', '<CR>', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+        { key = 'L', cb = tree_cb('cd') },
+        { key = '<C-s>', cb = tree_cb('split') },
+        { key = '<C-v>', cb = tree_cb('vsplit') },
+        { key = '<C-t>', cb = tree_cb('tabnew') },
+        { key = 'h', cb = tree_cb('close_node') },
+        { key = 'i', cb = tree_cb('preview') },
+        { key = 'R', cb = tree_cb('refresh') },
+        { key = 'c', cb = tree_cb('create') },
+        { key = 'D', cb = tree_cb('remove') },
+        { key = 'r', cb = tree_cb('rename') },
+        { key = 'd', cb = tree_cb('cut') },
+        { key = 'y', cb = tree_cb('copy') },
+        { key = 'p', cb = tree_cb('paste') },
+        { key = 'gyn', cb = tree_cb('copy_name') },
+        { key = 'gyp', cb = tree_cb('copy_path') },
+        {
+          key = 'gya',
+          cb = tree_cb('copy_absolute_path'),
+        },
+        { key = 'H', cb = tree_cb('dir_up') },
+        { key = 's', cb = tree_cb('system_open') },
+        { key = 'q', cb = tree_cb('close') },
+      },
+    },
+  },
+})
 
 map('n', '<Space>f', '<Cmd>NvimTreeToggle<CR>', { silent = true })
 map('n', '<Space>F', '<Cmd>NvimTreeFindFile<CR>z.', { silent = true })
