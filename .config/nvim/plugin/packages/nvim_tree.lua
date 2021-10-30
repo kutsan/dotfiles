bbbd9a82a96ceecee8f96e2ff2e6263fc7904571
Kutsan Kaplan <me@kutsan.dev>
2021-10-31 02:03:05 +0300

    refactor(nvim): remove hide_root_folder default setting for nvim-tree

--- .config/nvim/plugin/packages/nvim_tree.lua
+++ .config/nvim/plugin/packages/nvim_tree.lua
@@ -45,7 +45,6 @@ g.nvim_tree_special_files = {}
 local nvim_tree = require('nvim-tree')
 nvim_tree.setup({
   view = {
-    hide_root_folder = false,
     auto_resize = true,
     mappings = {
       custom_only = true,
