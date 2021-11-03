f8424eede42c9815bf811998183516f251b8e080
Kutsan Kaplan <me@kutsan.dev>
2021-11-04 00:21:50 +0300

    refactor(nvim): remove fugitive mappings in favor of fzf-lua

--- .config/nvim/plugin/packages/fugitive.lua
+++ .config/nvim/plugin/packages/fugitive.lua
@@ -1,8 +1,5 @@
 local map = require('kutsan/utils').map
 
 -- Define mappings.
-map('n', '<Space>gs', '<Cmd>Git<CR>', { silent = true })
 map('n', '<Space>gb', '<Cmd>Git blame<CR>', { silent = true })
 map('n', '<Space>gd', '<Cmd>Gdiffsplit<CR>', { silent = true })
-map('n', '<Space>gl', '<Cmd>0Gclog!<CR>', { silent = true })
-map('n', '<Space>gL', '<Cmd>Gclog!<CR>', { silent = true })
