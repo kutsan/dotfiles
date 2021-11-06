9b67bd8ef688015a57dc18b7af0b20b693a7d965
Kutsan Kaplan <me@kutsan.dev>
2021-11-07 00:56:43 +0300

    feat(nvim): add construct search and replace without :cdo

--- .config/nvim/plugin/mappings/command.lua
+++ .config/nvim/plugin/mappings/command.lua
@@ -10,5 +10,6 @@ map('c', ';s', 'substitute/\\v//gc<Left><Left><Left><Left>')
 -- Construct global command with 'very magic' mode.
 map('c', ';g', 'global/\\v/<Left>')
 
--- Construct search and replace with populated quickfix list.
-map('c', ';r', 'cdo substitute/<C-r>=@/<CR>//gce<Left><Left><Left><Left>')
+-- Construct search and replace.
+map('c', ';r', 'substitute/<C-r>=@/<CR>//gce<Left><Left><Left><Left>')
+map('c', ';R', 'cdo substitute/<C-r>=@/<CR>//gce<Left><Left><Left><Left>')
