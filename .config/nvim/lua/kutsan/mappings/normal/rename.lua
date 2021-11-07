ecd21303efa3b90e4996eabac70a654cd78f2505
Kutsan Kaplan <me@kutsan.dev>
2021-11-07 22:44:15 +0300

    refactor(nvim): drop plenary dependency of rename function

--- .config/nvim/lua/kutsan/mappings/normal/rename.lua
+++ .config/nvim/lua/kutsan/mappings/normal/rename.lua
@@ -1,22 +1,24 @@
 local buf_map = require('kutsan/utils').buf_map
-local popup = require('plenary/popup')
+local api = vim.api
 local fn = vim.fn
 local cmd = vim.cmd
+local lsp = vim.lsp
 
-local function rename()
+local function open_rename_window()
   local current_name = fn.expand('<cword>')
 
-  local window = popup.create(current_name, {
-    style = 'minimal',
-    borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+  local buffer = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_lines(buffer, 0, -1, true, { current_name })
+
+  local window = api.nvim_open_win(buffer, true, {
     relative = 'cursor',
-    borderhighlight = 'FloatBorder',
-    titlehighlight = 'Title',
+    border = 'rounded',
     focusable = true,
     width = 30,
     height = 1,
-    line = 'cursor+2',
-    col = 'cursor-1',
+    col = 0,
+    row = 1,
+    style = 'minimal',
   })
 
   cmd('startinsert!')
@@ -26,7 +28,10 @@ local function rename()
   buf_map(
     'i',
     '<CR>',
-    ('<Cmd>stopinsert | lua _rename(\'"%s"\')<CR>'):format(current_name),
+    ('<Cmd>stopinsert | lua require("kutsan/mappings/normal/rename").rename_symbol_from_window(%d, "%s")<CR>'):format(
+      window,
+      current_name
+    ),
     {
       silent = true,
     }
@@ -34,39 +39,40 @@ local function rename()
   buf_map(
     'n',
     '<CR>',
-    ('<Cmd>stopinsert | lua _rename(\'"%s"\')<CR>'):format(current_name),
+    ('<Cmd>stopinsert | lua require("kutsan/mappings/normal/rename").rename_symbol_from_window(%d, "%s")<CR>'):format(
+      window,
+      current_name
+    ),
     {
       silent = true,
     }
   )
+end
 
-  function _G._rename(current)
-    local new_name = vim.trim(fn.getline('.'))
-    vim.api.nvim_win_close(window, true)
+local function rename_symbol_from_window(window, current_name)
+  local new_name = vim.trim(api.nvim_get_current_line())
+  api.nvim_win_close(window, true)
 
-    local lsp_method = 'textDocument/rename'
+  local lsp_method = 'textDocument/rename'
 
-    if #new_name > 0 and new_name ~= current then
-      local params = vim.lsp.util.make_position_params()
-      params.newName = new_name
+  if #new_name > 0 and new_name ~= current_name then
+    local params = lsp.util.make_position_params()
+    params.newName = new_name
 
-      vim.lsp.buf_request(
-        0,
-        lsp_method,
-        params,
-        function(err, result, ctx, config)
-          if err then
-            vim.notify(
-              ("Error running lsp query '%s': %s"):format(lsp_method, err),
-              vim.log.levels.ERROR
-            )
-          end
+    lsp.buf_request(0, lsp_method, params, function(err, result, ctx, config)
+      if err then
+        vim.notify(
+          ("Error running lsp query '%s': %s"):format(lsp_method, err),
+          vim.log.levels.ERROR
+        )
+      end
 
-          vim.lsp.handlers[lsp_method](err, result, ctx, config)
-        end
-      )
-    end
+      lsp.handlers[lsp_method](err, result, ctx, config)
+    end)
   end
 end
 
-return { rename = rename }
+return {
+  open_rename_window = open_rename_window,
+  rename_symbol_from_window = rename_symbol_from_window,
+}
