86356e22f38309d46cee5f5e226b9a01fa0bcfa3
Kutsan Kaplan <me@kutsan.dev>
2021-11-19 23:53:18 +0300

    feat(nvim): add filetype_exclude to indent_blankline plugin

--- .config/nvim/plugin/packages/indent_blankline.lua
+++ .config/nvim/plugin/packages/indent_blankline.lua
@@ -1,5 +1,6 @@
 local indent_blankline = require('indent_blankline')
 
 indent_blankline.setup({
-  buftype_exclude = { 'terminal' },
+  buftype_exclude = { 'terminal', 'nofile' },
+  filetype_exclude = { 'help' },
 })
