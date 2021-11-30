4ca925de7c1e0711a45c31575f023030db958971
Kutsan Kaplan <me@kutsan.dev>
2021-11-30 23:46:40 +0300

    feat(nvim): add man to filetype_exclude of indent_blankline

--- .config/nvim/plugin/packages/indent_blankline.lua
+++ .config/nvim/plugin/packages/indent_blankline.lua
@@ -2,5 +2,5 @@ local indent_blankline = require('indent_blankline')
 
 indent_blankline.setup({
   buftype_exclude = { 'terminal', 'nofile' },
-  filetype_exclude = { 'help' },
+  filetype_exclude = { 'help', 'man' },
 })
