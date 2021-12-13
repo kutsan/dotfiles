1346669517b7141139046451b1b345eca909e6bc
Kutsan Kaplan <me@kutsan.dev>
2021-12-13 10:00:09 +0300

    style(nvim): fix cmp.lua formatting

--- .config/nvim/plugin/packages/cmp.lua
+++ .config/nvim/plugin/packages/cmp.lua
@@ -24,7 +24,8 @@ cmp.setup({
         return col ~= 0
           and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
               :sub(col, col)
-              :match('%s') == nil
+              :match('%s')
+            == nil
       end
 
       if cmp.visible() then
