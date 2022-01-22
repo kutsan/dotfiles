82780a75b04b5999826a3ee849c7261ebfa49cb0
Kutsan Kaplan <me@kutsan.dev>
2022-01-22 19:01:28 +0300

    feat(nvim): disable multi window support of pounce

--- .config/nvim/plugin/packages/pounce.lua
+++ .config/nvim/plugin/packages/pounce.lua
@@ -1,4 +1,9 @@
+local pounce = require('pounce')
 local map = require('kutsan/utils').map
+
+pounce.setup({
+  multi_window = false,
+})
 
 map('n', '/', '<Cmd>Pounce<CR>')
 map('n', '?', '<Cmd>Pounce<CR>')
