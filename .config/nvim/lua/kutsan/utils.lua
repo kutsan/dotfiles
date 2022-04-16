26c0b0b9615eb561da6f106443748315c68e2d50
Kutsan Kaplan <me@kutsan.dev>
2022-04-16 21:58:19 +0300

    fix(nvim): first argument of nvim_buf_set_keymap accept only 0 as current buffer

--- .config/nvim/lua/kutsan/utils.lua
+++ .config/nvim/lua/kutsan/utils.lua
@@ -18,7 +18,7 @@ local function buf_map(mode, lhs, rhs, opts_overrides)
     opts[key] = value
   end
 
-  api.nvim_buf_set_keymap(nil, mode, lhs, rhs, opts)
+  api.nvim_buf_set_keymap(0, mode, lhs, rhs, opts)
 end
 
 local function set_undo_ftplugin(cmd)
