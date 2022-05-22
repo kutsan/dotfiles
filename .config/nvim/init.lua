9913cad8a019b2108f1c07962767d299b896b719
Kutsan Kaplan <me@kutsan.dev>
2022-05-22 22:56:13 +0300

    refactor(nvim): move devicons setup inside devicons.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,3 @@
-local devicons = require('nvim-web-devicons')
 local g = vim.g
 
 -- Enable Lua filetype detection.
@@ -18,19 +17,3 @@ g.loaded_getscriptPlugin = true
 g.loaded_logipat = true
 g.loaded_tutor_mode_plugin = true
 g.loaded_matchit = true
-
--- Set devicons overrides early.
-devicons.setup({
-  override = {
-    html = { icon = '', color = '#e67e22', name = 'Html' },
-    css = { icon = '', color = '#689fb6', name = 'Css' },
-    js = { icon = '', color = '#f5c06f', name = 'Js' },
-    jsx = { icon = '', color = '#689fb6', name = 'Jsx' },
-    md = { icon = '', color = '#44788e', name = 'Md' },
-    png = { icon = '', color = '#d4843e', name = 'Png' },
-    webp = { icon = '', color = '#3498db', name = 'Webp' },
-    jpg = { icon = '', color = '#16a085', name = 'Jpg' },
-    svg = { icon = '', color = '#3affdb', name = 'Svg' },
-    LICENSE = { icon = '', color = '#f1f1f1', name = 'License' },
-  },
-})
