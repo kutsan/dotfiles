e6470cc759ae896d7ec10d1addac177726d3f637
Kutsan Kaplan <me@kutsan.dev>
2022-06-13 18:47:32 +0300

    refactor(nvim): update deprecated config for diffview

--- .config/nvim/plugin/packages/diffview.lua
+++ .config/nvim/plugin/packages/diffview.lua
@@ -3,9 +3,11 @@ local keymap = vim.keymap
 
 diffview.setup({
   file_panel = {
-    position = 'bottom',
-    height = 10,
     listing_style = 'tree',
+    win_config = {
+      position = 'bottom',
+      height = 10,
+    },
     tree_options = {
       flatten_dirs = false,
       folder_statuses = 'only_folded',
