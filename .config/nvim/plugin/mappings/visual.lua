2fd8e64e29c1d50323ac9ebfaa3f8a7ae67a8d97
Kutsan Kaplan <me@kutsan.dev>
2022-06-17 19:43:49 +0300

    refactor(nvim): remove visual selection mappings since neovim support it now

--- .config/nvim/plugin/mappings/visual.lua
+++ .config/nvim/plugin/mappings/visual.lua
@@ -8,18 +8,6 @@ keymap.set('x', '>', '>gv')
 keymap.set('x', '/', '/\\v')
 keymap.set('x', '?', '?\\v')
 
--- Start a * or # search from a visual selection.
-keymap.set(
-  'x',
-  '*',
-  '<Cmd>lua require("kutsan/mappings/visual/search").set_search("/")<CR>/<C-R>=@/<CR><CR>'
-)
-keymap.set(
-  'x',
-  '#',
-  '<Cmd>lua require("kutsan/mappings/visual/search").set_search("?")<CR>?<C-R>=@/<CR><CR>'
-)
-
 -- Refactor selection.
 keymap.set('x', '<Space>c*', '*Ncgn', { noremap = false })
 keymap.set('x', '<Space>c#', '#cgN', { noremap = false })
