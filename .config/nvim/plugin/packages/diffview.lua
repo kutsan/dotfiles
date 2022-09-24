b2945d74d9509e88c6bb091c502d42bc56873c16
Kutsan Kaplan <me@kutsan.dev>
2022-09-24 21:17:56 +0300

    refactor(nvim): use diffview to view history of changes

--- .config/nvim/plugin/packages/diffview.lua
+++ .config/nvim/plugin/packages/diffview.lua
@@ -16,7 +16,13 @@ diffview.setup({
 })
 
 keymap.set('n', '<Space>gvo', '<Cmd>DiffviewOpen<CR>', { silent = true })
-keymap.set('n', '<Space>gvl', '<Cmd>DiffviewFileHistory<CR>', { silent = true })
+keymap.set(
+  'n',
+  '<Space>gl',
+  '<Cmd>DiffviewFileHistory %<CR>',
+  { silent = true }
+)
+keymap.set('n', '<Space>gL', '<Cmd>DiffviewFileHistory<CR>', { silent = true })
 
 -- TODO: Add generic tab mappings and replace this with :tabclose.
 keymap.set('n', '<Space>gvq', '<Cmd>DiffviewClose<CR>', { silent = true })
