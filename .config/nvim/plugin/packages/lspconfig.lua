070ad15552b42f72d8864f1340eff4d583d024c3
Kutsan Kaplan <me@kutsan.dev>
2022-10-11 19:52:02 +0300

    fix(nvim): use dynamic_command instead of command for null-ls

--- .config/nvim/plugin/packages/lspconfig.lua
+++ .config/nvim/plugin/packages/lspconfig.lua
@@ -200,13 +200,13 @@ null_ls.setup({
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.stylelint.with({
-      command = command_resolver.from_node_modules(),
+      dynamic_command = command_resolver.from_node_modules(),
     }),
 
     null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.diagnostics.luacheck,
     null_ls.builtins.diagnostics.stylelint.with({
-      command = command_resolver.from_node_modules(),
+      dynamic_command = command_resolver.from_node_modules(),
     }),
 
     null_ls.builtins.code_actions.eslint_d,
