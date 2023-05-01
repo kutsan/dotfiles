babf895851c281b4b1e9ea74f57e582f77980974
Kutsan Kaplan <me@kutsan.dev>
2023-05-01 17:41:21 +0300

    refactor(nvim): use new on_attach event for nvim-tree

--- .config/nvim/plugin/packages/nvim_tree.lua
+++ .config/nvim/plugin/packages/nvim_tree.lua
@@ -1,7 +1,114 @@
-local keymap = vim.keymap
 local nvim_tree = require('nvim-tree')
+local nvim_tree_api = require('nvim-tree.api')
+local keymap = vim.keymap
+
+local function on_attach(bufnr)
+  local function opts(desc)
+    return {
+      desc = ('nvim-tree: %s'):format(desc),
+      buffer = bufnr,
+      silent = true,
+    }
+  end
+
+  vim.keymap.set('n', 'l', nvim_tree_api.node.open.edit, opts('Open'))
+  vim.keymap.set('n', '<CR>', nvim_tree_api.node.open.edit, opts('Open'))
+  vim.keymap.set(
+    'n',
+    '<2-LeftMouse>',
+    nvim_tree_api.node.open.edit,
+    opts('Open')
+  )
+  vim.keymap.set('n', 'L', nvim_tree_api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set(
+    'n',
+    '<C-s>',
+    nvim_tree_api.node.open.horizontal,
+    opts('Open: Horizontal Split')
+  )
+  vim.keymap.set(
+    'n',
+    '<C-v>',
+    nvim_tree_api.node.open.vertical,
+    opts('Open: Vertical Split')
+  )
+  vim.keymap.set(
+    'n',
+    '<C-t>',
+    nvim_tree_api.node.open.tab,
+    opts('Open: New Tab')
+  )
+  vim.keymap.set(
+    'n',
+    'h',
+    nvim_tree_api.node.navigate.parent_close,
+    opts('Close Directory')
+  )
+  vim.keymap.set(
+    'n',
+    '<Tab>',
+    nvim_tree_api.node.open.preview,
+    opts('Open Preview')
+  )
+  vim.keymap.set('n', 'R', nvim_tree_api.tree.reload, opts('Refresh'))
+  vim.keymap.set('n', 'c', nvim_tree_api.fs.create, opts('Create'))
+  vim.keymap.set('n', 'D', nvim_tree_api.fs.trash, opts('Trash'))
+  vim.keymap.set('n', 'r', nvim_tree_api.fs.rename, opts('Rename'))
+  vim.keymap.set(
+    'n',
+    '<C-r>',
+    nvim_tree_api.fs.rename_sub,
+    opts('Rename: Omit Filename')
+  )
+  vim.keymap.set('n', 'd', nvim_tree_api.fs.cut, opts('Cut'))
+  vim.keymap.set('n', 'y', nvim_tree_api.fs.copy.node, opts('Copy'))
+  vim.keymap.set('n', 'p', nvim_tree_api.fs.paste, opts('Paste'))
+  vim.keymap.set(
+    'n',
+    'P',
+    nvim_tree_api.marks.bulk.move,
+    opts('Move Bookmarked')
+  )
+  vim.keymap.set('n', 'f', nvim_tree_api.live_filter.start, opts('Filter'))
+  vim.keymap.set('n', '-', nvim_tree_api.tree.collapse_all, opts('Collapse'))
+  vim.keymap.set('n', 'gyn', nvim_tree_api.fs.copy.filename, opts('Copy Name'))
+  vim.keymap.set(
+    'n',
+    'gyp',
+    nvim_tree_api.fs.copy.relative_path,
+    opts('Copy Relative Path')
+  )
+  vim.keymap.set(
+    'n',
+    'gya',
+    nvim_tree_api.fs.copy.absolute_path,
+    opts('Copy Absolute Path')
+  )
+  vim.keymap.set('n', 'H', nvim_tree_api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', 's', nvim_tree_api.node.run.system, opts('Run System'))
+  vim.keymap.set(
+    'n',
+    '<Space>',
+    nvim_tree_api.marks.toggle,
+    opts('Toggle Bookmark')
+  )
+  vim.keymap.set('n', 'K', nvim_tree_api.node.show_info_popup, opts('Info'))
+  vim.keymap.set(
+    'n',
+    '<',
+    nvim_tree_api.node.navigate.sibling.first,
+    opts('First Sibling')
+  )
+  vim.keymap.set(
+    'n',
+    '>',
+    nvim_tree_api.node.navigate.sibling.last,
+    opts('Last Sibling')
+  )
+end
 
 nvim_tree.setup({
+  on_attach = on_attach,
   update_cwd = true,
   hijack_cursor = true,
   git = {
@@ -14,41 +121,6 @@ nvim_tree.setup({
   },
   view = {
     width = 32,
-    mappings = {
-      custom_only = true,
-      list = {
-        { key = { 'l', '<CR>', '<2-LeftMouse>' }, action = 'edit' },
-        { key = 'L', action = 'cd' },
-        { key = '<C-s>', action = 'split' },
-        { key = '<C-v>', action = 'vsplit' },
-        { key = '<C-t>', action = 'tabnew' },
-        { key = 'h', action = 'close_node' },
-        { key = '<Tab>', action = 'preview' },
-        { key = 'R', action = 'refresh' },
-        { key = 'c', action = 'create' },
-        { key = 'D', action = 'trash' },
-        { key = 'r', action = 'rename' },
-        { key = '<C-r>', action = 'full_rename' },
-        { key = 'd', action = 'cut' },
-        { key = 'y', action = 'copy' },
-        { key = 'p', action = 'paste' },
-        { key = 'P', action = 'bulk_move' },
-        { key = 'f', action = 'live_filter' },
-        { key = '-', action = 'collapse_all' },
-        { key = 'gyn', action = 'copy_name' },
-        { key = 'gyp', action = 'copy_path' },
-        {
-          key = 'gya',
-          action = 'copy_absolute_path',
-        },
-        { key = 'H', action = 'dir_up' },
-        { key = 's', action = 'system_open' },
-        { key = '<Space>', action = 'toggle_mark' },
-        { key = 'K', action = 'toggle_file_info' },
-        { key = '<', action = 'first_sibling' },
-        { key = '>', action = 'last_sibling' },
-      },
-    },
   },
   renderer = {
     highlight_git = true,
