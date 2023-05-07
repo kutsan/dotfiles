b47a9ff8eaf6dc01c8e85fbde90c8d0397e78ee3
Kutsan Kaplan <me@kutsan.dev>
2023-05-08 00:25:28 +0300

    feat(nvim): make autopairs work nicely with cmp

--- .config/nvim/plugin/packages/autopairs.lua
+++ .config/nvim/plugin/packages/autopairs.lua
@@ -1,3 +1,7 @@
 local autopairs = require('nvim-autopairs')
+local autopairs_cmp_completion = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
 
 autopairs.setup({})
+
+cmp.event:on('confirm_done', autopairs_cmp_completion.on_confirm_done())
