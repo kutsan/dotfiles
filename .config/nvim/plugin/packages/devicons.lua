44da40d83f0af29acaea3d64d6a35fcde5a08a12
Kutsan Kaplan <me@kutsan.dev>
2023-08-07 22:07:46 +0300

    refactor(nvim): update devicons overrides

--- .config/nvim/plugin/packages/devicons.lua
+++ .config/nvim/plugin/packages/devicons.lua
@@ -3,15 +3,13 @@ local devicons = require('nvim-web-devicons')
 -- Set devicons overrides early.
 devicons.setup({
   override = {
-    html = { icon = '', color = '#e67e22', name = 'Html' },
-    css = { icon = '', color = '#689fb6', name = 'Css' },
-    js = { icon = '', color = '#f5c06f', name = 'Js' },
+    js = { icon = '󰌞', color = '#f5c06f', name = 'Js' },
     jsx = { icon = '', color = '#689fb6', name = 'Jsx' },
-    md = { icon = '', color = '#44788e', name = 'Md' },
-    png = { icon = '', color = '#d4843e', name = 'Png' },
-    webp = { icon = '', color = '#3498db', name = 'Webp' },
-    jpg = { icon = '', color = '#16a085', name = 'Jpg' },
-    svg = { icon = '', color = '#3affdb', name = 'Svg' },
-    LICENSE = { icon = '', color = '#f1f1f1', name = 'License' },
+    ts = { icon = '󰛦', color = '#4377c1', name = 'Ts' },
+    tsx = { icon = '', color = '#4377c1', name = 'Tsx' },
+    png = { icon = '󰋩', color = '#d4843e', name = 'Png' },
+    webp = { icon = '󰋩', color = '#3498db', name = 'Webp' },
+    jpg = { icon = '󰋩', color = '#16a085', name = 'Jpg' },
+    svg = { icon = '󰋩', color = '#3affdb', name = 'Svg' },
   },
 })
