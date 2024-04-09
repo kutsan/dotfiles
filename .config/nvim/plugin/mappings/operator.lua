ef4d29a75c2ab87c3e8a7076c94ca7b671e05c36
Kutsan Kaplan <me@kutsan.dev>
2024-04-09 19:05:32 +0300

    refactor(nvim): remove comment mappings to support built-in commenting

--- .config/nvim/plugin/mappings/operator.lua
+++ .config/nvim/plugin/mappings/operator.lua
@@ -1,19 +1,1 @@
 local keymap = vim.keymap
-
--- Comment and uncomment operator.
-keymap.set(
-  'n',
-  'gc',
-  '<Cmd>set operatorfunc=kutsan#mappings#operator#comment#<CR>g@',
-  {
-    silent = true,
-  }
-)
-keymap.set(
-  'x',
-  'gc',
-  ':<C-u>call kutsan#mappings#operator#comment#(visualmode())<CR>',
-  {
-    silent = true,
-  }
-)
