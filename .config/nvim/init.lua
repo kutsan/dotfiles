95a05cd02d16668fa9815ac6047712dcf3aad0d0
Kutsan Kaplan <me@kutsan.dev>
2024-04-20 21:57:33 +0300

    refactor(nvim): remove vim.loop from lazy init code

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,13 +19,13 @@ g.loaded_matchit = true
 
 -- Load lazy.nvim.
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    '--branch=stable',
     lazypath,
   })
 end
