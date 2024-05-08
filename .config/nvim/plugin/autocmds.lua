a9b22fa33795c99b24589d56fce0d9f33c0d7ac0
Kutsan Kaplan <me@kutsan.dev>
2024-05-08 05:29:04 +0300

    feat(nvim): improve auto close nvim-tree code

--- .config/nvim/plugin/autocmds.lua
+++ .config/nvim/plugin/autocmds.lua
@@ -34,18 +34,31 @@ api.nvim_create_autocmd('BufReadPost', {
 })
 
 -- Closes neovim automatically when nvim-tree is the last window in the view.
--- api.nvim_create_autocmd('BufEnter', {
---   group = api.nvim_create_augroup('AutoCloseNvimTree', { clear = true }),
---   nested = true,
---   callback = function()
---     if
---         #api.nvim_list_wins() == 1
---         and require('nvim-tree.utils').is_nvim_tree_buf()
---     then
---       cmd.quit()
---     end
---   end,
--- })
+vim.api.nvim_create_autocmd('QuitPre', {
+  callback = function()
+    local tree_wins = {}
+    local floating_wins = {}
+    local wins = vim.api.nvim_list_wins()
+
+    for _, w in ipairs(wins) do
+      local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(w))
+
+      if bufname:match('NvimTree_') ~= nil then
+        table.insert(tree_wins, w)
+      end
+
+      if vim.api.nvim_win_get_config(w).relative ~= '' then
+        table.insert(floating_wins, w)
+      end
+    end
+
+    if 1 == #wins - #floating_wins - #tree_wins then
+      for _, w in ipairs(tree_wins) do
+        vim.api.nvim_win_close(w, true)
+      end
+    end
+  end,
+})
 
 -- -- Save the current buffer after changes.
 -- api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged', }, {
