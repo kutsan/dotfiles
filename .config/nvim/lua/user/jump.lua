ea22eb4e1c06f26cf180bd3356dd9c65de337e02
Kutsan Kaplan <me@kutsan.dev>
2024-08-06 23:44:22 +0300

    fix(nvim): correct quotes in jump_last_pos

--- .config/nvim/lua/user/jump.lua
+++ .config/nvim/lua/user/jump.lua
@@ -3,7 +3,7 @@ local function jump_last_pos()
     vim.bo.buftype == ''
     and vim.fn.index({ 'diff', 'gitcommit' }, vim.bo.filetype, 0, true) == -1
   then
-    if vim.fn.line("'\"") >= 1 and vim.fn.line("'\"") <= vim.fn.line('$') then
+    if vim.fn.line('\'"') >= 1 and vim.fn.line('\'"') <= vim.fn.line('$') then
       vim.cmd('normal! g`"zz')
     end
   end
