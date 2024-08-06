4b254454a2b7ff4f7e93805a986593f4f9c6ea9f
Kutsan Kaplan <me@kutsan.dev>
2024-08-06 22:59:43 +0300

    refactor(zsh): simplify zsh aliases

--- .config/zsh/config/aliases.zsh
+++ .config/zsh/config/aliases.zsh
@@ -1,21 +1,15 @@
 # Core Shortcuts
 alias rm='trash' # Use `trash` program instead of built-in irrecoverable way to delete nodes.
-alias mv='mv --interactive --verbose' # Move nodes with interactive mode and extra verbosity.
-alias cp='cp --interactive --verbose' # Copy nodes with interactive mode and extra verbosity.
-alias ln='ln --interactive --verbose' # Link nodes with interactive mode and extra verbosity.
+alias mv='mv -iv' # Move nodes with interactive mode and extra verbosity.
+alias ln='ln -iv' # Link nodes with interactive mode and extra verbosity.
 alias mkdir='mkdir -p' # Make missing parent directories when creating folders.
 alias grep="grep --color='auto'" # Grep with colors.
-alias du='du --max-depth=1 --si' # Display size of files and folders under current directory.
-alias df='df --all --si --print-type' # Display all disk usage statistics with SI units and FS types.
-alias ls='ls --almost-all --classify --color=always --group-directories-first --literal' # List name of nodes.
-alias la='ls -l --almost-all --si' # List nodes with their details.
+alias du='du -d 1 --si' # Display size of files and folders under current directory.
+alias df='df -a --si' # Display all disk usage statistics with SI units and FS types.
+alias ls='ls -AFG' # List name of nodes.
+alias la='ls -lAh' # List nodes with their details.
 
 # Custom Shortcuts
 alias v="$EDITOR"
 alias g='git'
-
-# git
-foreach key (a b bl c d ds dt dts f l ld r rd s sm sw) {
-  alias g$key="git $key"
-}
-unset key
+alias gs='git s'
