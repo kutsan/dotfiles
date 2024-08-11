aaafa985c481f7242382453040c38eb1767d3c23
Kutsan Kaplan <me@kutsan.dev>
2024-08-12 02:10:26 +0300

    refactor(zsh): remove RSA SSH key generator

--- .config/zsh/config/functions.zsh
+++ .config/zsh/config/functions.zsh
@@ -12,6 +12,3 @@ function lf() {
 function gen-ssh-keygen-ed25519() {
   ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/keys/$1 -C "$2"
 }
-function gen-ssh-keygen-rsa() {
-  ssh-keygen -t rsa -o -a 100 -b 4096 -f ~/.ssh/keys/$1 -C "$2"
-}
