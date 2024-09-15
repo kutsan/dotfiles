77efc2cdaabf5c7f106cc3b5b4990c1e04791b58
Kutsan Kaplan <me@kutsan.dev>
2024-09-16 02:41:00 +0300

    feat(nvim): add git blame mapping for git signs

--- .config/nvim/lua/specs/gitsigns.lua
+++ .config/nvim/lua/specs/gitsigns.lua
@@ -2,6 +2,15 @@ local Plugin = { 'lewis6991/gitsigns.nvim' }
 
 Plugin.name = 'gitsigns'
 
+Plugin.opts = {
+  on_attach = function()
+    local gitsigns = require('gitsigns')
+    local keymap = vim.keymap
+
+    keymap.set('n', '<Space>gb', gitsigns.blame, { silent = true })
+  end,
+}
+
 Plugin.config = true
 
 return Plugin
