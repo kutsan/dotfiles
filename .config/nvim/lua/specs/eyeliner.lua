91112f008d82b8bad63994387716d783ddd88ae3
Kutsan Kaplan <me@kutsan.dev>
2024-10-21 01:37:44 +0300

    feat(nvim): add eyeliner plugin

---
+++ .config/nvim/lua/specs/eyeliner.lua
@@ -0,0 +1,13 @@
+local Plugin = { 'jinh0/eyeliner.nvim' }
+
+Plugin.name = 'eyeliner'
+
+Plugin.config = function()
+  local eyeliner = require('eyeliner')
+
+  eyeliner.setup({
+    highlight_on_key = true,
+  })
+end
+
+return Plugin
