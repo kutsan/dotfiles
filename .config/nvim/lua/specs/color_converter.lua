ab007b2eab0c8c49467a3496a81a81b9882223d9
Kutsan Kaplan <me@kutsan.dev>
2025-04-01 06:45:19 +0300

    feat(nvim): add color_converter plugin

---
+++ .config/nvim/lua/specs/color_converter.lua
@@ -0,0 +1,20 @@
+local Plugin = { 'NTBBloodbath/color-converter.nvim' }
+
+Plugin.name = 'color-converter'
+Plugin.opts = {}
+
+Plugin.keys = {
+  {
+    '<Space>cc',
+    function()
+      local color_converter = require('color-converter')
+      color_converter.cycle()
+
+      -- Go back to the original position after conversion.
+      vim.api.nvim_feedkeys('``', 'n', true)
+    end,
+    desc = 'Color Converter',
+  },
+}
+
+return Plugin
