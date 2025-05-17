658999499792e76d48b35f18d40b49aa86669249
Kutsan Kaplan <me@kutsan.dev>
2025-05-17 20:05:20 +0300

    feat: add toml prettier plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,8 @@
-export { config as default } from '@kutsan/prettier-config'
+import { config as prettierConfig } from '@kutsan/prettier-config'
+
+const config = {
+  ...prettierConfig,
+  plugins: [...prettierConfig.plugins, 'prettier-plugin-toml'],
+}
+
+export default config
