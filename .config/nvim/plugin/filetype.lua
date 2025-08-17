c0f88fd74f034643c8767a8275416d5861c5e10b
Kutsan Kaplan <me@kutsan.dev>
2025-08-17 19:08:54 +0300

    feat(nvim): use correct file type for docker compose files

--- .config/nvim/plugin/filetype.lua
+++ .config/nvim/plugin/filetype.lua
@@ -4,3 +4,11 @@ vim.filetype.add({ pattern = { ['.*/doc/.-%.txt'] = 'help' } })
 
 -- .env files.
 vim.filetype.add({ pattern = { ['.env.*'] = 'config' } })
+
+-- Docker Compose files.
+vim.filetype.add({
+  pattern = {
+    ['docker%-compose.*%.ya?ml'] = 'yaml.docker-compose',
+    ['compose.*%.yaml'] = 'yaml.docker-compose',
+  },
+})
