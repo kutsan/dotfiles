aac263f17c4ec8eeb8f5a8149cf7f18393c2337a
Kutsan Kaplan <me@kutsan.dev>
2025-08-22 22:47:11 +0300

    feat: add git config file types

--- .config/nvim/plugin/filetype.lua
+++ .config/nvim/plugin/filetype.lua
@@ -5,6 +5,9 @@ vim.filetype.add({ pattern = { ['.*/doc/.-%.txt'] = 'help' } })
 -- .env files.
 vim.filetype.add({ pattern = { ['.env.*'] = 'config' } })
 
+-- Git configs.
+vim.filetype.add({ pattern = { ['.*%.gitconfig'] = 'gitconfig' } })
+
 -- Docker Compose files.
 vim.filetype.add({
   pattern = {
