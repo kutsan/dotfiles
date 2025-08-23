626c420284e2388734224d67ca7603dc4255b1d6
Kutsan Kaplan <me@kutsan.dev>
2025-08-23 20:42:44 +0300

    feat(nvim): add gui config

---
+++ .config/nvim/plugin/gui.lua
@@ -0,0 +1,14 @@
+local opt = vim.opt
+
+-- GUI Options
+opt.guifont = 'MonoLisa Variable,Symbols Nerd Font Mono:h17.5:#h-none'
+opt.linespace = 11
+
+-- Neovide Options
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0.05
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_fullscreen = true
+  vim.g.neovide_floating_corner_radius = 0.1
+end
