7140684dcc00345a7bda901c369eff802b98d696
Kutsan Kaplan <me@kutsan.dev>
2025-08-23 20:46:42 +0300

    feat(nvim): use prettier as a fallback formatter for conform

--- .config/nvim/lua/specs/conform.lua
+++ .config/nvim/lua/specs/conform.lua
@@ -2,14 +2,20 @@ local Plugin = { 'stevearc/conform.nvim' }
 
 Plugin.name = 'conform'
 
+local prettier_formatters = {
+  'prettierd',
+  'prettier',
+  stop_after_first = true,
+}
+
 Plugin.opts = {
   formatters_by_ft = {
     lua = { 'stylua' },
-    javascript = { 'prettierd' },
-    typescript = { 'prettierd' },
-    typescriptreact = { 'prettierd' },
-    yaml = { 'prettierd' },
-    json = { 'prettierd' },
+    javascript = prettier_formatters,
+    typescript = prettier_formatters,
+    typescriptreact = prettier_formatters,
+    yaml = prettier_formatters,
+    json = prettier_formatters,
   },
 }
 
