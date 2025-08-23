5a058927816beee923ed3c752023c6ebb4a07f35
Kutsan Kaplan <me@kutsan.dev>
2025-08-23 20:43:01 +0300

    feat(nvim): add schema_store plugin as a standalone plugin

---
+++ .config/nvim/lua/specs/schema_store.lua
@@ -0,0 +1,30 @@
+local Plugin = { 'b0o/SchemaStore.nvim' }
+
+Plugin.name = 'schema-store'
+
+Plugin.config = function()
+  local schema_store = require('schemastore')
+
+  vim.lsp.config('jsonls', {
+    settings = {
+      json = {
+        validate = { enable = true },
+        schemas = schema_store.json.schemas(),
+      },
+    },
+  })
+
+  vim.lsp.config('yamlls', {
+    settings = {
+      yaml = {
+        schemaStore = {
+          enable = false,
+          url = '',
+        },
+        schemas = schema_store.yaml.schemas(),
+      },
+    },
+  })
+end
+
+return Plugin
