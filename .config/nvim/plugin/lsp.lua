5ec3ebf8d74071b64782b13ea783ef7fdad0c7a7
Kutsan Kaplan <me@kutsan.dev>
2026-01-09 11:07:37 +0300

    feat(nvim): add inline completion mappings

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -1,71 +1,123 @@
-local function on_attach(client)
-	if client.server_capabilities.documentHighlightProvider then
-		local document_highlight_autocmd_group =
-			vim.api.nvim_create_augroup('DocumentHighlight', { clear = true })
+local lsp_group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true })
 
-		vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-			group = document_highlight_autocmd_group,
-			buffer = 0,
-			callback = function()
-				if client.server_capabilities.documentHighlightProvider then
-					vim.lsp.buf.document_highlight()
-				end
-			end,
-		})
-
-		vim.api.nvim_create_autocmd('CursorMoved', {
-			group = document_highlight_autocmd_group,
-			buffer = 0,
-			callback = function()
-				if client.server_capabilities.documentHighlightProvider then
-					vim.lsp.buf.clear_references()
-				end
-			end,
-		})
-	end
-
-	local map_opts = {
-		buffer = true,
-		silent = true,
-	}
-
-	if vim.lsp.inlay_hint then
-		vim.keymap.set('n', 'coi', function()
-			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-		end, map_opts)
-	end
-
-	vim.keymap.set('n', 'K', function()
-		local width = math.floor(vim.o.columns * 0.8)
-		local height = math.floor(vim.o.lines * 0.3)
-
-		vim.lsp.buf.hover({
-			max_height = height,
-			max_width = width,
-		})
-	end)
-
-	vim.keymap.set('n', 'gd', function()
-		vim.lsp.buf.definition()
-	end, map_opts)
-
-	vim.keymap.set('n', 'gD', function()
-		vim.lsp.buf.declaration()
-	end, map_opts)
-
-	vim.keymap.set({ 'n', 'x' }, '\\f', function()
-		if client.server_capabilities.documentFormattingProvider then
-			vim.lsp.buf.format()
+-- Highlight references to the symbol under the cursor.
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = lsp_group,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return
 		end
 
-		require('conform').format()
-	end, map_opts)
+		if
+			client:supports_method(
+				vim.lsp.protocol.Methods.textDocument_documentHighlight,
+				args.buf
+			)
+		then
+			local highlight_group =
+				vim.api.nvim_create_augroup('LspDocumentHighlight', { clear = false })
 
-	vim.keymap.set('n', 'J', function()
-		vim.diagnostic.open_float()
-	end, map_opts)
-end
+			-- Clear existing highlights for this buffer.
+			vim.api.nvim_clear_autocmds({ buffer = args.buf, group = highlight_group })
 
-vim.lsp.config('*', {
-	on_attach = on_attach,
+			vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				group = highlight_group,
+				buffer = args.buf,
+				callback = vim.lsp.buf.document_highlight,
+			})
+
+			vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+				group = highlight_group,
+				buffer = args.buf,
+				callback = vim.lsp.buf.clear_references,
+			})
+		end
+	end,
+})
+
+-- General keymaps for LSP functions.
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = lsp_group,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		local map_opts = { buffer = args.buf, silent = true }
+
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, map_opts)
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, map_opts)
+		vim.keymap.set('n', 'J', vim.diagnostic.open_float, map_opts)
+		vim.keymap.set('n', 'K', function()
+			local width = math.floor(vim.o.columns * 0.8)
+			local height = math.floor(vim.o.lines * 0.3)
+			vim.lsp.buf.hover({ max_height = height, max_width = width })
+		end, map_opts)
+
+		vim.keymap.set({ 'n', 'x' }, '\\f', function()
+			if
+				client
+				and client:supports_method(
+					vim.lsp.protocol.Methods.textDocument_formatting,
+					args.buf
+				)
+			then
+				vim.lsp.buf.format()
+			end
+
+			require('conform').format()
+		end, map_opts)
+	end,
+})
+
+-- Keymaps for inlay hints.
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = lsp_group,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return
+		end
+
+		if
+			client:supports_method(
+				vim.lsp.protocol.Methods.textDocument_inlayHint,
+				args.buf
+			) and vim.lsp.inlay_hint
+		then
+			vim.keymap.set('n', 'coi', function()
+				vim.lsp.inlay_hint.enable(
+					not vim.lsp.inlay_hint.is_enabled({ bufnr = args.buf }),
+					{ bufnr = args.buf }
+				)
+			end, { buffer = args.buf, silent = true, desc = 'Toggle Inlay Hints' })
+		end
+	end,
+})
+
+-- Keymaps for inlay completion.
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = lsp_group,
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then
+			return
+		end
+
+		if
+			client:supports_method(
+				vim.lsp.protocol.Methods.textDocument_inlineCompletion,
+				args.buf
+			)
+		then
+			vim.lsp.inline_completion.enable(true, { bufnr = args.buf })
+
+			vim.keymap.set('i', '<M-l>', vim.lsp.inline_completion.get, {
+				desc = 'LSP: accept inline completion',
+				buffer = args.buf,
+			})
+			vim.keymap.set('i', '<M-]>', vim.lsp.inline_completion.select, {
+				desc = 'LSP: switch inline completion',
+				buffer = args.buf,
+			})
+		end
+	end,
 })
