4047eda057ee17606212715a3dd83023a66721c7
Kutsan Kaplan <me@kutsan.dev>
2026-01-11 03:47:55 +0300

    refactor(nvim): do not use treesitter for folding and indentation

--- .config/nvim/lua/specs/treesitter.lua
+++ .config/nvim/lua/specs/treesitter.lua
@@ -70,9 +70,6 @@ Plugin.opts = {
 	},
 	sync_install = false,
 	auto_install = true,
-	indent = {
-		enable = true,
-	},
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -121,8 +121,7 @@ opt.linebreak = true -- Wrap long lines at a character in 'breakat'.
 opt.number = true -- Print the line number in front of each line.
 opt.cursorline = true -- Highlight the screen line of the cursor with CursorLine.
 opt.signcolumn = 'yes:1' -- Always draw the sign column including sign space even there is no sign in it.
-opt.foldmethod = 'expr' -- Use custom fold expression.
-opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()' -- Custom fold expression for treesitter.
+opt.foldmethod = 'indent' -- Use indentation levels for folding.
 opt.foldnestmax = 4 -- Maximum nesting of folds.
 opt.numberwidth = 4 -- Minimum number column width.
 
