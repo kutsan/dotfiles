c01f9a96b6990fe705f78fd91bd3623e77cd6c74
Kutsan Kaplan <me@kutsan.dev>
2026-01-31 21:42:18 +0300

    refactor(zsh): remove zinit auto-update function

--- home/dot_config/exact_zsh/exact_config/plugins.zsh
+++ home/dot_config/exact_zsh/exact_config/plugins.zsh
@@ -11,23 +11,3 @@ zinit wait lucid for light-mode kutsan/zsh-system-clipboard
 zinit wait lucid \
 	atload"FAST_HIGHLIGHT_STYLES[\${FAST_THEME_NAME}unknown-token]='fg=red'" \
 	for light-mode zdharma-continuum/fast-syntax-highlighting
-
-# Auto-update plugins once per week.
-function _zinit_autoupdate() {
-	zmodload -F zsh/datetime +p:EPOCHSECONDS
-
-	local update_file="$XDG_CACHE_HOME/zinit/zinit-last-update"
-	local last_update=0
-
-	if ([[ -f "$update_file" ]]) {
-		last_update=$(<"$update_file")
-	}
-
-	# Update if more than 7 days have passed since last update.
-	if (( EPOCHSECONDS - last_update > 60 * 60 * 24 * 7 )) {
-		echo "$EPOCHSECONDS" >! "$update_file"
-		zinit update --all --no-pager
-	}
-}
-_zinit_autoupdate
-unfunction _zinit_autoupdate
