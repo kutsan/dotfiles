6941b163aa082cddc26d5978492de3eda8ca481c
Kutsan Kaplan <me@kutsan.dev>
2026-02-17 01:27:26 +0300

    feat(zsh): update git hashes of zsh plugins

--- home/dot_config/exact_zsh/exact_config/plugins.zsh
+++ home/dot_config/exact_zsh/exact_config/plugins.zsh
@@ -1,13 +1,5 @@
-##
-# Get commit hashes using:
-#
-# ```sh
-# for d in $ZINIT[PLUGINS_DIR]/*(/); [[ -d $d/.git ]] && { u=$(git -C $d remote get-url origin); echo "${${u#https://github.com/}%.git}: \"$(git -C $d rev-parse HEAD)\"" }
-# ```
-##
-
 # pure: A minimal prompt asy Git and async support.
-zinit ice ver'54bd501c802283dee0940457da6eb3e642bd1453' pick'async.zsh' src'pure.zsh'
+zinit ice ver'dbefd0d' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
 
 # atuin: Shell history integration.
@@ -17,21 +9,21 @@ zinit ice as"command" from"gh-r" bpick"atuin-*.tar.gz" mv"atuin*/atuin -> atuin"
 zinit light atuinsh/atuin
 
 # zsh-autosuggestions: Fish-like auto-suggestions.
-zinit ice ver'85919cd1ffa7d2d5412f6d3fe437ebdbeeec4fc5'
+zinit ice ver'85919cd'
 zinit light zsh-users/zsh-autosuggestions
 
 # zsh-autopair: Insert or delete brackets, parens, quotes in pair.
 zinit wait lucid \
-	ver'449a7c3d095bc8f3d78cf37b9549f8bb4c383f3d' \
+	ver'449a7c3' \
 	for light-mode hlissner/zsh-autopair
 
 # zsh-system-clipboard: Synchronize system clipboard with ZLE registers.
 zinit wait lucid \
-	ver'5f1d497ee3c215a967c0e6b9a772e73c40332d52' \
+	ver'5f1d497' \
 	for light-mode kutsan/zsh-system-clipboard
 
 # fast-syntax-highlighting: Syntax highlighting support.
 zinit wait lucid \
-	ver'3d574ccf48804b10dca52625df13da5edae7f553' \
+	ver'3d574cc' \
 	atload"FAST_HIGHLIGHT_STYLES[\${FAST_THEME_NAME}unknown-token]='fg=red'" \
 	for light-mode zdharma-continuum/fast-syntax-highlighting
