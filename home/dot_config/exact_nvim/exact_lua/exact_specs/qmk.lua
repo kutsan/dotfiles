000e8e20f011dc085aada96f6c22108f2d69e281
Kutsan Kaplan <me@kutsan.dev>
2026-02-17 01:30:46 +0300

    refactor(nvim): disable generating qmk comments

--- home/dot_config/exact_nvim/exact_lua/exact_specs/qmk.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_specs/qmk.lua
@@ -4,6 +4,9 @@ Plugin.name = 'qmk'
 
 Plugin.opts = {
 	name = 'LAYOUT_60_ansi',
+	comment_preview = {
+		position = 'none',
+	},
 	layout = {
 		'x x x x x x x x x x x x x x',
 		'x x x x x x x x x x x x x x',
