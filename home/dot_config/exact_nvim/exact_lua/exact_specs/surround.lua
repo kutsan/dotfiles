44bd1f02ddfd3370e4a7add58e7a8e2f571af1c6
Kutsan Kaplan <me@kutsan.dev>
2026-03-01 19:44:26 +0300

    refactor(nvim): explicitly define key mappings for surround

--- home/dot_config/exact_nvim/exact_lua/exact_specs/surround.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_specs/surround.lua
@@ -5,15 +5,28 @@ Plugin.version = '*'
 
 Plugin.event = 'VeryLazy'
 
-Plugin.opts = {
-	keymaps = {
-		normal = 'sa',
-		visual = 'sa',
-		delete = 'sd',
-		change = 'sr',
-		insert = false,
-		insert_line = false,
-	},
-}
+Plugin.opts = {}
+
+Plugin.init = function(_, opts)
+	local surround = require('nvim-surround')
+
+	-- Disable default key mappings.
+	vim.g.nvim_surround_no_normal_mappings = true
+
+	surround.setup(opts)
+
+	vim.keymap.set('n', 'sa', '<Plug>(nvim-surround-normal)', {
+		desc = 'Add a surrounding pair around a motion',
+	})
+	vim.keymap.set('x', 'sa', '<Plug>(nvim-surround-visual)', {
+		desc = 'Add a surrounding pair around a visual selection',
+	})
+	vim.keymap.set('n', 'sd', '<Plug>(nvim-surround-delete)', {
+		desc = 'Delete a surrounding pair',
+	})
+	vim.keymap.set('n', 'sr', '<Plug>(nvim-surround-change)', {
+		desc = 'Change a surrounding pair',
+	})
+end
 
 return Plugin
