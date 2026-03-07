988101200158010e1a938951f9995a225d4a2658
Kutsan Kaplan <me@kutsan.dev>
2026-03-07 23:53:36 +0300

    revert: "refactor(nvim): remove copilot plugin in favor of minuet"

---
+++ home/dot_config/exact_nvim/exact_lua/exact_specs/copilot.lua
@@ -0,0 +1,22 @@
+local Plugin = { 'zbirenbaum/copilot.lua' }
+
+Plugin.name = 'copilot'
+
+Plugin.cmd = 'Copilot'
+Plugin.event = 'InsertEnter'
+
+Plugin.opts = {
+	panel = { enabled = false },
+	suggestion = {
+		auto_trigger = true,
+		keymap = {
+			accept_word = '<M-w>',
+		},
+	},
+	filetypes = {
+		gitcommit = true,
+		markdown = true,
+	},
+}
+
+return Plugin
