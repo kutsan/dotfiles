49e4a2316c736a81dab491c6b10cf55281185720
Kutsan Kaplan <me@kutsan.dev>
2026-03-17 22:02:50 +0300

    feat(nvim): add blink lazydev integration

--- home/dot_config/exact_nvim/exact_lua/exact_specs/blink.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_specs/blink.lua
@@ -44,7 +44,14 @@ Plugin.opts = {
 		enabled = true,
 	},
 	sources = {
-		default = { 'lsp', 'path', 'snippets', 'buffer' },
+		default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+		providers = {
+			lazydev = {
+				name = 'LazyDev',
+				module = 'lazydev.integrations.blink',
+				score_offset = 100,
+			},
+		},
 	},
 	fuzzy = {
 		implementation = 'prefer_rust_with_warning',
