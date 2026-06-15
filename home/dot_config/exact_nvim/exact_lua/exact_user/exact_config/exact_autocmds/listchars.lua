a65ffc23159776c201f4b60b771a2830ea1cf226
Kutsan Kaplan <me@kutsan.dev>
2026-06-16 02:25:00 +0300

    feat(nvim): sync listchars multispace/leadmultispace with shiftwidth

---
+++ home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_autocmds/listchars.lua
@@ -0,0 +1,37 @@
+local function sync_multispace_listchars()
+	local shiftwidth = vim.bo.shiftwidth
+
+	if shiftwidth < 2 then
+		return
+	end
+
+	local lead = '▏' .. string.rep(' ', shiftwidth - 1)
+
+	vim.opt_local.listchars:append({
+		multispace = lead,
+		leadmultispace = lead,
+	})
+end
+
+vim.api.nvim_create_autocmd('OptionSet', {
+	desc = 'Sync listchars multispace/leadmultispace with shiftwidth (e.g. from .editorconfig).',
+	pattern = 'shiftwidth',
+	group = vim.api.nvim_create_augroup(
+		'SyncMultiSpaceListchars',
+		{ clear = true }
+	),
+	callback = function()
+		vim.schedule(sync_multispace_listchars)
+	end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'FileType' }, {
+	desc = 'Apply multispace/leadmultispace on buffer/window entry as a fallback for OptionSet.',
+	group = vim.api.nvim_create_augroup(
+		'SyncMultiSpaceListcharsFallback',
+		{ clear = true }
+	),
+	callback = function()
+		vim.schedule(sync_multispace_listchars)
+	end,
+})
