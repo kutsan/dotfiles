9f032595c6f04b4bc473c81fad0f7cd969377928
Kutsan Kaplan <me@kutsan.dev>
2026-06-27 23:40:53 +0300

    feat(mise): update shellcheck script

--- tasks/check/shellcheck.sh
+++ tasks/check/shellcheck.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+shopt -s inherit_errexit nullglob
+IFS=$'\n\t'
 
 find_static_files() {
 	find . \
@@ -25,28 +27,29 @@ find_template_files() {
 }
 
 find_rendered_files() {
-	local tmpdir=$1
+	local -r tmpdir=$1
 	find "$tmpdir" -type f -print0
 }
 
 find_all_files() {
-	local tmpdir=$1
+	local -r tmpdir=$1
 	find_static_files
 	find_rendered_files "$tmpdir"
 }
 
 create_chezmoi_config() {
-	local tmpconfig=$1
+	local -r tmpconfig=$1 source_root=$2
 
 	chezmoi execute-template \
 		--init \
 		--stdinisatty=false \
-		<"$MISE_PROJECT_ROOT/home/.chezmoi.toml.tmpl" \
+		<"$source_root/home/.chezmoi.toml.tmpl" \
 		>"$tmpconfig"
 }
 
 render_templates() {
-	local tmpdir=$1 tmpconfig=$2 file dest
+	local -r tmpdir=$1 tmpconfig=$2 source_root=$3
+	local file dest
 
 	while IFS= read -r -d '' file; do
 		dest="$tmpdir/${file%.tmpl}"
@@ -54,19 +57,19 @@ render_templates() {
 
 		# TODO: Find a better way to lint OS-specific templates without overriding `.chezmoi.os`.
 		sed 's/\.chezmoi\.os/"darwin"/g' "$file" |
-			chezmoi --source "$MISE_PROJECT_ROOT" --config "$tmpconfig" execute-template >"$dest"
+			chezmoi --source "$source_root" --config "$tmpconfig" execute-template >"$dest"
 	done
 }
 
 normalize_paths() {
-	local tmpdir=$1
+	local -r tmpdir=$1
 	sed "s|$tmpdir/||g"
 }
 
 main() {
-	local tmpdir=$1 tmpconfig=$2
+	local -r tmpdir=$1 tmpconfig=$2 source_root=$3
 
-	find_template_files | render_templates "$tmpdir" "$tmpconfig"
+	render_templates "$tmpdir" "$tmpconfig" "$source_root" < <(find_template_files)
 
 	find_all_files "$tmpdir" |
 		xargs -0 shellcheck --color=always -- |
@@ -74,8 +77,10 @@ main() {
 }
 
 tmpdir=$(mktemp -d)
+readonly tmpdir
 tmpconfig=$(mktemp "${TMPDIR:-/tmp}/chezmoi.XXXXXX.toml")
+readonly tmpconfig
 trap 'rm -rf -- "$tmpdir"; rm -f -- "$tmpconfig"' EXIT INT TERM
 
-create_chezmoi_config "$tmpconfig"
-main "$tmpdir" "$tmpconfig"
+create_chezmoi_config "$tmpconfig" "$MISE_PROJECT_ROOT"
+main "$tmpdir" "$tmpconfig" "$MISE_PROJECT_ROOT"
