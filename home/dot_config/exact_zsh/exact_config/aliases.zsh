31a881b90b9190ff09dab6ef72a29b8baab8affa
Kutsan Kaplan <me@kutsan.dev>
2026-07-09 00:06:23 +0300

    refactor(zsh): remove claude alias in favor of sbx

--- home/dot_config/exact_zsh/exact_config/aliases.zsh
+++ home/dot_config/exact_zsh/exact_config/aliases.zsh
@@ -53,9 +53,6 @@ alias px='pnpx'
 alias cz='chezmoi'
 alias bb='brew bundle --global --verbose'
 
-# AI Agents
-alias claude='sbx run claude --kit $XDG_CONFIG_HOME/sbx/kits/claude'
-
 # Git
 alias g='git'
 compdef g='git'
