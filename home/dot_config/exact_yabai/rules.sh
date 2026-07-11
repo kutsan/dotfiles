10db7c8f92e515df09dbb841c79a17cb52d53313
Kutsan Kaplan <me@kutsan.dev>
2026-07-12 01:14:28 +0300

    refactor(yabai): improve rule management

--- home/dot_config/exact_yabai/rules.sh
+++ home/dot_config/exact_yabai/rules.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 
-unmanaged_apps=(
-	'System Settings'
-	'Activity Monitor'
-	'Calculator'
-	'Dictionary'
-	'Disk Utility'
-	'QuickTime Player'
-	'FaceTime'
-	'iD'
-)
+set -euo pipefail
+IFS=$'\n\t'
 
-unmanaged_windows=(
-	'Google Chrome' 'Meet - '
-)
+# Standard positions (mirror skhd float-snap vocabulary)
+readonly grid_base="12:12"
+readonly center="$grid_base:2:1:8:10"
+readonly left_half="$grid_base:1:1:5:10"
 
-for app in "${unmanaged_apps[@]}"; do
-	yabai --message rule --add app="^$app\$" manage=off
-done
+yabai --message rule --add manage=off grid="$center" \
+	app="^(System Settings|Activity Monitor|Disk Utility)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(Calculator|Dictionary)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(QuickTime Player|FaceTime)$"
 
-for ((i = 0; i < ${#unmanaged_windows[@]}; i += 2)); do
-	app=${unmanaged_windows[i]}
-	title=${unmanaged_windows[i + 1]}
+yabai --message rule --add manage=off grid="$center" \
+	app="^(Mail|Notes)$"
+yabai --message rule --add manage=off grid="$left_half" \
+	app="^Reminders$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^Home$"
 
-	yabai --message rule --add app="^$app\$" title="^$title" manage=off
-done
+yabai --message rule --add manage=off grid="$center" \
+	app="^iD$"
+
+yabai --message rule --add manage=off grid="$center" space=9 \
+	app="^Slack$"
+
+yabai --message rule --add manage=off \
+	app="^Google Chrome$" title="^Meet - "
+
+yabai --message rule --apply
