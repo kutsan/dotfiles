201e66f2cdd0e7f8952fb9767256c6bdf168fca0
Kutsan Kaplan <me@kutsan.dev>
2026-07-12 16:07:30 +0300

    feat(yabai): add rules for additional apps and adjust window positions

--- home/dot_config/exact_yabai/rules.sh
+++ home/dot_config/exact_yabai/rules.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Standard positions (mirror skhd float-snap vocabulary)
+# Standard positions (mirror `skhd` float-snap vocabulary)
 readonly grid_base="12:12"
 readonly center="$grid_base:2:1:8:10"
-readonly left_half="$grid_base:1:1:5:10"
+readonly right_half="$grid_base:6:1:5:10"
 
 yabai --message rule --add manage=off grid="$center" \
 	app="^(System Settings|Activity Monitor|Disk Utility)$"
@@ -14,19 +14,24 @@ yabai --message rule --add manage=off grid="$center" \
 	app="^(Calculator|Dictionary)$"
 yabai --message rule --add manage=off grid="$center" \
 	app="^(QuickTime Player|FaceTime)$"
-
 yabai --message rule --add manage=off grid="$center" \
-	app="^(Mail|Notes)$"
-yabai --message rule --add manage=off grid="$left_half" \
+	app="^(Mail|Notes|Home)$"
+yabai --message rule --add manage=off grid="$right_half" \
 	app="^Reminders$"
-yabai --message rule --add manage=off grid="$center" \
-	app="^Home$"
 
 yabai --message rule --add manage=off grid="$center" \
-	app="^iD$"
+	app="^(IINA|HandBrake|HomePass)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(Backblaze|Pearcleaner|Parachute|Blackmagic Disk Speed Test)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(Docker Desktop|Transmission|Hyperduck|Codepoint)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(iD|Insta360 Link Controller|Ukelele)$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(WireGuard|Ledger Live)$"
 
-yabai --message rule --add manage=off grid="$center" space=9 \
-	app="^Slack$"
+yabai --message rule --add manage=off grid="$center" \
+	app="^(Slack|Discord|WhatsApp)$"
 
 yabai --message rule --add manage=off \
 	app="^Google Chrome$" title="^Meet - "
