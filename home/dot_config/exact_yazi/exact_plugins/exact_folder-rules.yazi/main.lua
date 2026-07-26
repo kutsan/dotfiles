7b4a154e3b89f9d1734eb83f2ea5a5063c18120e
Kutsan Kaplan <me@kutsan.dev>
2026-07-26 17:56:02 +0300

    refactor(yazi): update config according to the breaking changes

--- home/dot_config/exact_yazi/exact_plugins/exact_folder-rules.yazi/main.lua
+++ home/dot_config/exact_yazi/exact_plugins/exact_folder-rules.yazi/main.lua
@@ -3,9 +3,9 @@ local function setup()
 		local cwd = cx.active.current.cwd
 
 		if cwd:ends_with('Downloads') then
-			ya.mgr_emit('sort', { 'mtime', reverse = true, dir_first = false })
+			ya.emit('sort', { 'mtime', reverse = true, dir_first = false })
 		else
-			ya.mgr_emit('sort', { 'natural', reverse = false, dir_first = true })
+			ya.emit('sort', { 'natural', reverse = false, dir_first = true })
 		end
 	end)
 end
