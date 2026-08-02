1e72ed1bf401e413b2dca47dcca19340d68a3cc7
Kutsan Kaplan <me@kutsan.dev>
2026-08-02 15:38:46 +0300

    feat(yabai): add padding for external bar

--- home/dot_config/exact_yabai/exact_config/settings.sh
+++ home/dot_config/exact_yabai/exact_config/settings.sh
@@ -3,16 +3,18 @@
 set -o errexit -o nounset -o pipefail
 IFS=$'\n\t'
 
+readonly space_padding=10
+readonly window_gap=8
+readonly external_bar_padding=40
+
 yabai --message config \
 	layout bsp \
-	window_placement second_child
-
-readonly padding=10
-readonly gap=8
+	window_placement second_child \
+	external_bar all:$((external_bar_padding - space_padding)):0
 
 yabai --message config \
-	top_padding "$padding" \
-	bottom_padding "$padding" \
-	left_padding "$padding" \
-	right_padding "$padding" \
-	window_gap "$gap"
+	top_padding "$space_padding" \
+	bottom_padding "$space_padding" \
+	left_padding "$space_padding" \
+	right_padding "$space_padding" \
+	window_gap "$window_gap"
