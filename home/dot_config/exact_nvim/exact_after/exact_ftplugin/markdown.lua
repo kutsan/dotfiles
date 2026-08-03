95fea1cc10b56e79696273353457aa38ce275114
Kutsan Kaplan <me@kutsan.dev>
2026-08-04 00:34:12 +0300

    feat(nvim): soft wrap markdown files by default

--- home/dot_config/exact_nvim/exact_after/exact_ftplugin/markdown.lua
+++ home/dot_config/exact_nvim/exact_after/exact_ftplugin/markdown.lua
@@ -4,4 +4,5 @@ local set_undo_ftplugin =
 set_undo_ftplugin(function()
 	vim.opt_local.number = nil
 	vim.opt_local.relativenumber = nil
+	vim.opt_local.wrap = nil
 end)
--- home/dot_config/exact_nvim/exact_ftplugin/markdown.lua
+++ home/dot_config/exact_nvim/exact_ftplugin/markdown.lua
@@ -3,3 +3,6 @@ local opt_local = vim.opt_local
 -- Disable all line numbers.
 opt_local.number = false
 opt_local.relativenumber = false
+
+-- Soft wrap long lines.
+opt_local.wrap = true
