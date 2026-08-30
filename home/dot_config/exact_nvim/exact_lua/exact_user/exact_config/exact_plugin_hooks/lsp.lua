25d62ee95b8f993495e42a4d3a635d6f2966537b
Kutsan Kaplan <me@kutsan.dev>
2026-08-30 20:57:52 +0300

    refactor(nvim): organize plugin hooks folder

---
+++ home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_plugin_hooks/lsp.lua
@@ -0,0 +1,19 @@
+-- Update mason tools after `mason-tool-installer` is updated.
+vim.api.nvim_create_autocmd('PackChanged', {
+	callback = function(event)
+		local name = event.data.spec.name
+		local kind = event.data.kind
+
+		if name ~= 'mason-tool-installer' then
+			return
+		end
+
+		if kind ~= 'update' or not package.loaded['mason-tool-installer'] then
+			return
+		end
+
+		local mason_tool_installer = require('mason-tool-installer')
+		mason_tool_installer.check_install(true)
+		mason_tool_installer.clean()
+	end,
+})
