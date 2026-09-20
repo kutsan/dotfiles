b1754dc8d2adc4553c5f9f60e112e7cbc6130bd5
Kutsan Kaplan <me@kutsan.dev>
2026-09-20 22:22:21 +0300

    refactor(nvim): use poisonbloom file name for colorscheme

--- home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_colorschemes/init.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_colorschemes/init.lua
@@ -1,3 +1,4 @@
-require('user.config.colorschemes.catppuccin')
+require('user.config.colorschemes.poisonbloom')
 
+-- `catppuccin` is used only as a backend for Poisonbloom. See `poisonbloom.lua` for the color palette.
 vim.cmd.colorscheme('catppuccin')
