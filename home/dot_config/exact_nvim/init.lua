f91d177e86e5542988f83feb411c493aa4fa9d1a
Kutsan Kaplan <me@kutsan.dev>
2026-09-20 20:23:26 +0300

    feat(nvim): extend ui2 config

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -4,7 +4,32 @@ vim.loader.enable()
 -- Enable the new experimental UI for Neovim.
 require('vim._core.ui2').enable({
 	msg = {
-		targets = 'msg',
+		targets = {
+			emsg = 'pager',
+			echoerr = 'pager',
+			lua_error = 'pager',
+			rpc_error = 'pager',
+			shell_err = 'pager',
+			shell_out = 'pager',
+			list_cmd = 'pager',
+			verbose = 'pager',
+
+			empty = 'cmd',
+			completion = 'cmd',
+			search_count = 'cmd',
+			wildlist = 'cmd',
+			typed_cmd = 'cmd',
+		},
+		cmd = {
+			height = 0.5,
+		},
+		msg = {
+			height = 0.3,
+			timeout = 5000,
+		},
+		pager = {
+			height = 0.5,
+		},
 	},
 })
 
