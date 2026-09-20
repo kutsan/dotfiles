220a89dd7553ba2408c7fd3735ae1e6eefa155b6
Kutsan Kaplan <me@kutsan.dev>
2026-09-20 22:11:47 +0300

    feat(nvim): use poisonbloom colorscheme instead of catppuccin colors

--- home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_colorschemes/catppuccin.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_user/exact_config/exact_colorschemes/catppuccin.lua
@@ -1,4 +1,5 @@
 vim.pack.add({
+	-- `catppuccin` is used only as a backend for Poisonbloom. Its default palette is not used.
 	{
 		name = 'catppuccin',
 		src = 'github:catppuccin/nvim',
@@ -7,6 +8,70 @@ vim.pack.add({
 
 local catppuccin = require('catppuccin')
 
+local poisonbloom = {
+	dark = {
+		crust = '#050505',
+		mantle = '#0e0f12',
+		base = '#080808',
+
+		surface0 = '#14161b',
+		surface1 = '#22262e',
+		surface2 = '#363f4e',
+		overlay0 = '#606876',
+		overlay1 = '#6a7282',
+		overlay2 = '#737b8b',
+		subtext0 = '#9ca3b0',
+		subtext1 = '#b3b8c2',
+		text = '#c9cdd4',
+
+		red = '#c8939c',
+		maroon = '#dab4ba',
+		flamingo = '#c9a6bd',
+		rosewater = '#b4c2da',
+		peach = '#c6ad9f',
+		yellow = '#c6bf9f',
+		green = '#90c1ac',
+		teal = '#afd4c5',
+		sky = '#95bec6',
+		sapphire = '#b5d3d9',
+		blue = '#93a6c8',
+		lavender = '#b4c2da',
+		mauve = '#b89dc8',
+		pink = '#ceb9d9',
+	},
+
+	light = {
+		crust = '#e3e5e8',
+		mantle = '#ebedef',
+		base = '#f5f5f5',
+
+		surface0 = '#dddfe3',
+		surface1 = '#cacdd3',
+		surface2 = '#b6bac3',
+		overlay0 = '#848d9f',
+		overlay1 = '#707a8f',
+		overlay2 = '#60697b',
+		subtext0 = '#484e5b',
+		subtext1 = '#3d424d',
+		text = '#31363f',
+
+		red = '#8d4c57',
+		maroon = '#763a44',
+		flamingo = '#874c74',
+		rosewater = '#384d71',
+		peach = '#7b5742',
+		yellow = '#695f39',
+		green = '#386955',
+		teal = '#2a5443',
+		sky = '#3b666e',
+		sapphire = '#2c525a',
+		blue = '#496088',
+		lavender = '#384d71',
+		mauve = '#794e92',
+		pink = '#643c7a',
+	},
+}
+
 local opts = {
 	flavour = 'auto',
 	background = {
@@ -14,16 +79,9 @@ local opts = {
 		light = 'latte',
 	},
 	no_bold = true,
-	term_colors = true,
 	color_overrides = {
-		latte = {
-			text = '#404359',
-		},
-		mocha = {
-			base = '#0f1119',
-			mantle = '#090b10',
-			crust = '#171b26',
-		},
+		latte = poisonbloom.light,
+		mocha = poisonbloom.dark,
 	},
 	highlight_overrides = {
 		all = function(colors)
@@ -31,7 +89,6 @@ local opts = {
 
 			return {
 				-- Built-in highlights
-				Whitespace = { fg = utils.darken(colors.surface0, 0.4, colors.base) },
 				LspInlayHint = {
 					fg = colors.overlay0,
 					bg = 'NONE',
@@ -50,30 +107,29 @@ local opts = {
 					bg = utils.darken(colors.sky, 0.4, colors.base),
 					fg = colors.text,
 				},
-				MsgArea = { bg = utils.darken(colors.surface0, 0.1, colors.base) },
-				MsgSeparator = { bg = utils.darken(colors.surface0, 0.1, colors.base) },
-				TabLineSel = { bg = colors.surface2 },
 
-				-- treesitter: TypeScript with JSX
-				['@constructor.tsx'] = { fg = colors.yellow },
-				['@tag.tsx'] = { fg = colors.red },
+				-- Plugin `snacks`
+				SnacksIndentScope = {
+					fg = utils.darken(colors.overlay0, 0.4, colors.base),
+				},
 
-				-- snacks
-				SnacksPickerTree = {
-					fg = utils.darken(colors.surface0, 0.7, colors.base),
+				-- Plugin `codediff`
+				NeoTreeIndentMarker = {
+					fg = utils.darken(colors.overlay0, 0.2, colors.base),
+				},
+				CodeDiffFiller = {
+					fg = utils.darken(colors.overlay0, 0.2, colors.base),
 				},
 			}
 		end,
 	},
 	integrations = {
-		diffview = true,
 		treesitter = true,
 		treesitter_context = true,
 		blink_cmp = {
 			enabled = true,
 		},
 		gitsigns = true,
-		noice = true,
 		snacks = {
 			enabled = true,
 		},
